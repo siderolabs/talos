40bb8a2b1dc891c299c98b3f332f14a6bd6800f3
Andrew Rynhard <andrew@andrewrynhard.com>
2018-03-14 07:47:53 -0700

    fix(init): typo in service subnet field; pin version of Kubernetes

--- initramfs/src/init/pkg/process/kubeadm.go
+++ initramfs/src/init/pkg/process/kubeadm.go
@@ -11,9 +11,10 @@ const MasterConfiguration = `
 kind: MasterConfiguration
 apiVersion: kubeadm.k8s.io/v1alpha1
 skipTokenPrint: true
+kubernetesVersion: v1.9.4
 networking:
   dnsDomain: cluster.local
-  ProcessSubnet: 10.96.0.0/12
+  serviceSubnet: 10.96.0.0/12
   podSubnet: 10.244.0.0/16
 featureGates:
   HighAvailability: true
