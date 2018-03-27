13a13ed817050f0498e0500e60414b569e4b0c60
Andrew Rynhard <andrew@andrewrynhard.com>
2018-03-26 22:31:31 -0700

    chore(rootfs): cleanup include and share directories (#28)

--- rootfs/src/cleanup.sh
+++ rootfs/src/cleanup.sh
@@ -16,3 +16,6 @@ remove_symlinks
 find /rootfs -type f -name \*.a -print0 | xargs -0 rm -rf
 find /rootfs/lib /rootfs/usr/lib -type f \( -name \*.so* -a ! -name \*dbg \) -exec strip --strip-unneeded {} ';'
 find /rootfs/{bin,sbin} /rootfs/usr/{bin,sbin,libexec} -type f -exec strip --strip-all {} ';'
+
+rm -rf /rootfs/usr/include/*
+rm -rf /rootfs/usr/share/*
