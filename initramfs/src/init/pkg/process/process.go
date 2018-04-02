c1848eeb7f9e77f6967e708c3986f562dd3df652
Andrew Rynhard <andrew@andrewrynhard.com>
2018-04-02 13:03:51 -0700

    fix(init): make log handling non-blocking

--- initramfs/src/init/pkg/process/process.go
+++ initramfs/src/init/pkg/process/process.go
@@ -2,15 +2,13 @@ package process
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os/exec"
 	"path"
-	"strings"
 	"time"
 
 	"github.com/autonomy/dianemo/initramfs/src/init/pkg/constants"
+	logstream "github.com/autonomy/dianemo/initramfs/src/init/pkg/log"
 )
 
 type Type int
@@ -20,8 +18,6 @@ const (
 	Once
 	OnceAndOnlyOnce
 )
-
-var outputstreams = map[string]*io.PipeReader{}
 
 type Process interface {
 	Cmd() (string, []string)
@@ -41,9 +37,10 @@ func (m *Manager) build(proc Process) (*exec.Cmd, error) {
 	cmd := exec.Command(name, args...)
 	// Set the environment for the process.
 	cmd.Env = append(proc.Env(), fmt.Sprintf("PATH=%s", constants.PATH))
-	// Create a buffer for the stdout and stderr of the process.
-	r, w := io.Pipe()
-	outputstreams[path.Base(name)] = r
+	w, err := logstream.New(path.Base(name))
+	if err != nil {
+		return nil, fmt.Errorf("process log handler: %s", err.Error())
+	}
 	cmd.Stdout = w
 	cmd.Stderr = w
 
@@ -68,36 +65,6 @@ func (m *Manager) Start(proc Process) error {
 	}(proc)
 
 	return nil
-}
-
-func StreamHandleFunc(w http.ResponseWriter, r *http.Request) {
-	name := strings.TrimPrefix(r.URL.Path, "/logs/")
-	stream, ok := outputstreams[name]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-	}
-
-	if stream == nil {
-		w.Write([]byte(fmt.Sprintf("process has not started: %s", name)))
-		return
-	}
-
-	buffer := make([]byte, 1024)
-	for {
-		n, err := stream.Read(buffer)
-		if err != nil {
-			stream.Close()
-			break
-		}
-		data := buffer[0:n]
-		w.Write(data)
-		if f, ok := w.(http.Flusher); ok {
-			f.Flush()
-		}
-		for i := 0; i < n; i++ {
-			buffer[i] = 0
-		}
-	}
 }
 
 func (m *Manager) waitAndRestart(proc Process) {
