4d005f00fb68315d5cbb9ae578da41c1b1110fe7
Andrew Rynhard <andrew@andrewrynhard.com>
2018-04-19 17:05:09 -0700

    refactor(init): use /root for new root path (#53)

--- initramfs/src/init/pkg/constants/constants.go
+++ initramfs/src/init/pkg/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	KernelRootFlag  = "dianemo.autonomy.io/root"
 	UserDataURLFlag = "dianemo.autonomy.io/userdata"
-	NewRoot         = "/mnt/switch_root"
+	NewRoot         = "/root"
 	DATALabel       = "DATA"
 	ROOTLabel       = "ROOT"
 	PATH            = "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/opt/cni/bin"
