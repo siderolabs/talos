cf760dbd12e1110edacaf12d57e6032657f08c31
Andrew Rynhard <andrew@andrewrynhard.com>
2018-07-23 21:17:15 -0700

    feat(initramfs): set the platform explicitly

--- src/initramfs/cmd/init/pkg/platform/platform.go
+++ src/initramfs/cmd/init/pkg/platform/platform.go
@@ -3,6 +3,10 @@
 package platform
 
 import (
+	"fmt"
+
+	"github.com/autonomy/dianemo/src/initramfs/cmd/init/pkg/constants"
+	"github.com/autonomy/dianemo/src/initramfs/cmd/init/pkg/kernel"
 	"github.com/autonomy/dianemo/src/initramfs/cmd/init/pkg/platform/baremetal"
 	"github.com/autonomy/dianemo/src/initramfs/cmd/init/pkg/platform/cloud/aws"
 	"github.com/autonomy/dianemo/src/initramfs/pkg/userdata"
@@ -17,11 +21,23 @@ type Platform interface {
 
 // NewPlatform is a helper func for discovering the current platform.
 func NewPlatform() (p Platform, err error) {
-	if aws.IsEC2() {
-		p = &aws.AWS{}
-	} else {
-		p = &baremetal.BareMetal{}
+	arguments, err := kernel.ParseProcCmdline()
+	if err != nil {
+		return
 	}
-
+	if platform, ok := arguments[constants.KernelParamPlatform]; ok {
+		switch platform {
+		case "aws":
+			if aws.IsEC2() {
+				p = &aws.AWS{}
+			} else {
+				return nil, fmt.Errorf("failed to verify EC2 PKCS7 signature")
+			}
+		case "bare-metal":
+			p = &baremetal.BareMetal{}
+		default:
+			return nil, fmt.Errorf("no platform specified")
+		}
+	}
 	return p, nil
 }
