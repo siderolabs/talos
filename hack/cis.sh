3fadca919122c3c2af5b14b4d642654731a65528
Andrew Rynhard <andrew@andrewrynhard.com>
2018-10-13 09:32:15 -0700

    chore(conformance): sonobuoy script and kube-bench job

--- hack/cis.sh
+++ hack/cis.sh
@@ -2,27 +2,40 @@
 
 set -eou pipefail
 
-SPEC_VERSION=${SPEC_VERSION:-"1.8"}
-IMAGE=${IMAGE:-"aquasec/kube-bench:latest"}
+NAMESPACE=${NAMESPACE:-"cis-kube-bench"}
 
 cleanup() {
-    kubectl delete pod ${POD_NAME} > /dev/null
+    kubectl delete ns ${NAMESPACE}
 }
 
 trap cleanup EXIT
 
+run_master_benchmark() {
+    JOB_NAME=kube-bench-master
+    kubectl apply -f cis-kube-bench-master.yaml -n ${NAMESPACE}
+    kubectl wait --timeout=60s --for=condition=complete job/${JOB_NAME} -n ${NAMESPACE} > /dev/null
+    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.log
+}
+
+run_node_benchmark() {
+    JOB_NAME=kube-bench-node
+    kubectl apply -f cis-kube-bench-node.yaml -n ${NAMESPACE}
+    kubectl wait --timeout=60s --for=condition=complete job/${JOB_NAME} -n ${NAMESPACE} > /dev/null
+    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.log
+}
+
+kubectl create ns ${NAMESPACE}
+
 case $1 in
 master)
-    POD_NAME="kube-bench-master"
-    kubectl run ${POD_NAME} --image=${IMAGE} --restart=Never --overrides="{ \"apiVersion\": \"v1\", \"spec\": { \"hostPID\": true, \"nodeSelector\": { \"node-role.kubernetes.io/master\": \"\" }, \"tolerations\": [ { \"key\": \"node-role.kubernetes.io/master\", \"operator\": \"Exists\", \"effect\": \"NoSchedule\" } ] } }" -- master --json --version ${SPEC_VERSION} > /dev/null
-    sleep 5
-    kubectl logs ${POD_NAME}
+    run_master_benchmark
     ;;
 node)
-    POD_NAME="kube-bench-node"
-    kubectl run ${POD_NAME} --image=${IMAGE} --restart=Never --overrides="{ \"apiVersion\": \"v1\", \"spec\": { \"hostPID\": true } }" -- node --json --version ${SPEC_VERSION} > /dev/null
-    sleep 5
-    kubectl logs ${POD_NAME}
+    run_node_benchmark
+    ;;
+all)
+    run_master_benchmark & run_node_benchmark
+    wait
     ;;
 *)
   ;;
---
+++ hack/sonobuoy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eou pipefail
+
+cleanup() {
+    sonobuoy delete > /dev/null
+}
+
+trap cleanup EXIT
+
+wait_for_results() {
+    kubectl wait --timeout=60s --for=condition=ready pod/sonobuoy -n heptio-sonobuoy
+    while sonobuoy status | grep 'Sonobuoy is still running'; do sleep 10; done
+    # Sleep in order to avoid 'error retrieving results: error: tmp/sonobuoy no such file or directory'
+    sleep 60
+    sonobuoy retrieve ../build
+}
+
+case $1 in
+conformance)
+    kubectl apply -f ./sonobuoy-conformance.yaml
+    wait_for_results
+    ;;
+quick)
+    kubectl apply -f ./sonobuoy-quick.yaml
+    wait_for_results
+    ;;
+*)
+  ;;
+esac
