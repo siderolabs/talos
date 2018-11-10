fc84b6218d742991aa67d2b6565c62833c9e43ed
Andrew Rynhard <andrew@andrewrynhard.com>
2018-11-10 15:43:35 -0800

    fix(hack): add /etc/kubernetes to CIS benchmark jobs (#199)

--- hack/cis.sh
+++ hack/cis.sh
@@ -14,14 +14,14 @@ run_master_benchmark() {
     JOB_NAME=kube-bench-master
     kubectl apply -f cis-kube-bench-master.yaml -n ${NAMESPACE}
     kubectl wait --timeout=60s --for=condition=complete job/${JOB_NAME} -n ${NAMESPACE} > /dev/null
-    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.log
+    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.json
 }
 
 run_node_benchmark() {
     JOB_NAME=kube-bench-node
     kubectl apply -f cis-kube-bench-node.yaml -n ${NAMESPACE}
     kubectl wait --timeout=60s --for=condition=complete job/${JOB_NAME} -n ${NAMESPACE} > /dev/null
-    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.log
+    kubectl logs job/${JOB_NAME} -n ${NAMESPACE} | jq . >../build/cis-${JOB_NAME}.json
 }
 
 kubectl create ns ${NAMESPACE}
