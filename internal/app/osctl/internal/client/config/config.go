6d8e94d293cb7213c34cd53bf6653fb39ce8c358
Andrew Rynhard <andrew@andrewrynhard.com>
2019-02-23 21:21:54 -0800

    fix: write config changes to specified config file (#416)
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- internal/app/osctl/internal/client/config/config.go
+++ internal/app/osctl/internal/client/config/config.go
@@ -6,8 +6,6 @@ package config
 
 import (
 	"io/ioutil"
-	"os/user"
-	"path"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -42,17 +40,13 @@ func Open(p string) (c *Config, err error) {
 }
 
 // Save writes the config to disk.
-func (c *Config) Save() (err error) {
-	u, err := user.Current()
-	if err != nil {
-		return
-	}
+func (c *Config) Save(p string) (err error) {
 	configBytes, err := yaml.Marshal(c)
 	if err != nil {
 		return
 	}
 
-	if err = ioutil.WriteFile(path.Join(u.HomeDir, ".talos", "config"), configBytes, 0600); err != nil {
+	if err = ioutil.WriteFile(p, configBytes, 0600); err != nil {
 		return
 	}
 
