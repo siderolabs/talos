a8fa1f5cd1483f3157228f0cc6341e232fc3882f
Andrew Rynhard <andrew@andrewrynhard.com>
2019-04-26 08:24:31 -0700

    feat(osctl): add df command (#569)
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

---
+++ cmd/osctl/cmd/df.go
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// nolint: dupl,golint
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/talos-systems/talos/cmd/osctl/pkg/client"
+	"github.com/talos-systems/talos/cmd/osctl/pkg/helpers"
+	"github.com/talos-systems/talos/internal/pkg/constants"
+)
+
+// dfCmd represents the df command.
+var dfCmd = &cobra.Command{
+	Use:   "df",
+	Short: "List disk usage",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		creds, err := client.NewDefaultClientCredentials(talosconfig)
+		if err != nil {
+			helpers.Fatalf("error getting client credentials: %s", err)
+		}
+		if target != "" {
+			creds.Target = target
+		}
+		c, err := client.NewClient(constants.OsdPort, creds)
+		if err != nil {
+			helpers.Fatalf("error constructing client: %s", err)
+		}
+
+		if err := c.DF(); err != nil {
+			helpers.Fatalf("error getting df: %s", err)
+		}
+	},
+}
+
+func init() {
+	dfCmd.Flags().StringVarP(&target, "target", "t", "", "target the specificed node")
+	rootCmd.AddCommand(dfCmd)
+}
