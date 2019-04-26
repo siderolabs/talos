2f6d5e02608c04d554d2fb2bdb6bed32aa1ff8a5
Andrew Rynhard <andrew@andrewrynhard.com>
2019-04-26 12:48:26 -0700

    feat: add package for generating userdata (#574)
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

---
+++ pkg/userdata/generate/admin.go
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package generate
+
+// Talosconfig returns the talos admin Talos config.
+func Talosconfig(in *Input) (string, error) {
+	return renderTemplate(in, talosconfigTempl)
+}
+
+const talosconfigTempl = `---
+context: {{ .ClusterName }}
+contexts:
+  {{ .ClusterName }}:
+    target: {{ index .MasterIPs 0 }}
+    ca: {{ .Certs.OsCert }}
+    crt: {{ .Certs.AdminCert }}
+    key: {{ .Certs.AdminKey }}
+`
---
+++ pkg/userdata/generate/controlplane.go
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package generate
+
+const controlPlaneTempl = `---
+version: ""
+security: null
+services:
+  init:
+    cni: flannel
+  kubeadm:
+    certificateKey: '{{ .KubeadmTokens.CertKey }}'
+    configuration: |
+      apiVersion: kubeadm.k8s.io/v1beta1
+      kind: JoinConfiguration
+      controlPlane:
+        apiEndpoint:
+          advertiseAddress: {{ index .MasterIPs .Index }}
+          bindPort: 6443
+      discovery:
+        bootstrapToken:
+          token: '{{ .KubeadmTokens.BootstrapToken }}'
+          unsafeSkipCAVerification: true
+          apiServerEndpoint: {{ index .MasterIPs 0 }}:443
+      nodeRegistration:
+        taints: []
+        kubeletExtraArgs:
+          node-labels: ""
+          feature-gates: ExperimentalCriticalPodAnnotation=true
+  trustd:
+    username: '{{ .TrustdInfo.Username }}'
+    password: '{{ .TrustdInfo.Password }}'
+    endpoints: [ "{{ index .MasterIPs 0 }}" ]
+    bootstrapNode: "{{ index .MasterIPs 0 }}"
+    certSANs: [ "{{ index .MasterIPs .Index }}" ]
+`
