d82444a42dfb854679904d919db00cd4944a2425
Andrew Rynhard <andrew@andrewrynhard.com>
2019-07-04 08:43:34 -0700

    chore: revert #816 (#829)
    This reverts commit 11f8392e53986da6c124037ffab06064ec04aedd.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- hack/test/basic-integration.sh
+++ hack/test/basic-integration.sh
@@ -9,13 +9,11 @@ export KUBECONFIG="${TMP}/kubeconfig"
 
 
 cleanup() {
-	${OSCTL} cluster destroy --name "${DRONE_COMMIT_SHA:0:7}"
+	${OSCTL} cluster destroy --name integration
 	rm -rf ${TMP}
 }
 trap cleanup EXIT
 
 ./hack/test/osctl-cluster-create.sh
-
-${OSCTL} config generate cluster.local 1.2.3.4,2.3.4.5,3.4.5.6
 
 exit 0
