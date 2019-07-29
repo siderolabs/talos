587011e2502083c14fd56a6052c9d45b8ef43191
Andrey Smirnov <smirnov.andrey@gmail.com>
2019-07-30 00:37:36 +0300

    chore: remove hack/dev/ scripts & docker-compose
    They are outdated, `osctl cluster` implements cluster up/down in a
    better way. K8s manifests are left intact, they are used in integration
    tests.
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- hack/dev/kubectl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -i --network dev_talosbr -v "${PWD}/kubeconfig":/root/.kube/config -v "${PWD}/manifests":/manifests k8s.gcr.io/hyperkube:${HYPERKUBE_TAG:-v1.14.1} kubectl $@
--- hack/dev/osctl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -i -v "${PWD}/../../build/osctl-linux-amd64:/bin/osctl:ro" -v "${PWD}/talosconfig:/root/.talos/config" --network dev_talosbr alpine osctl $@
