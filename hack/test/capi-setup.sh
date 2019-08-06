bfc1646cd9ac33567f77a171158c79c0df4688d7
Brad Beam <brad.beam@talos-systems.com>
2019-08-06 13:52:52 -0500

    chore(ci): Add e2e promotion pipeline
    Signed-off-by: Brad Beam <brad.beam@talos-systems.com>

--- hack/test/capi-setup.sh
+++ hack/test/capi-setup.sh
@@ -7,10 +7,10 @@ source ./hack/test/e2e-runner.sh
 mkdir -p $TMP
 
 ## Drop in capi stuff
-sed -i "s/{{PACKET_AUTH_TOKEN}}/${PACKET_AUTH_TOKEN}/" ${PWD}/hack/test/manifests/provider-components.yaml
-sed -i "s#{{GCE_SVC_ACCT}}#${GCE_SVC_ACCT}#" ${PWD}/hack/test/manifests/capi-secrets.yaml
-sed -i "s#{{AZURE_SVC_ACCT}}#${AZURE_SVC_ACCT}#" ${PWD}/hack/test/manifests/capi-secrets.yaml
-e2e_run "kubectl apply -f /e2emanifests/provider-components.yaml -f /e2emanifests/capi-secrets.yaml"
+sed "s/{{PACKET_AUTH_TOKEN}}/${PACKET_AUTH_TOKEN}/" ${PWD}/hack/test/manifests/provider-components.yaml > ${TMP}/provider-components.yaml
+sed -e "s#{{GCE_SVC_ACCT}}#${GCE_SVC_ACCT}#" \
+    -e "s#{{AZURE_SVC_ACCT}}#${AZURE_SVC_ACCT}#" ${PWD}/hack/test/manifests/capi-secrets.yaml > ${TMP}/capi-secrets.yaml
+e2e_run "kubectl apply -f ${TMP}/provider-components.yaml -f ${TMP}/capi-secrets.yaml"
 
 ## Wait for talosconfig in cm then dump it out
 e2e_run "timeout=\$((\$(date +%s) + ${TIMEOUT}))
