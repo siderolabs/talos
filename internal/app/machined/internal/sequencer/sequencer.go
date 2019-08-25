e1be06c40decc51af53bc60dbaae6b8b295291b4
Andrew Rynhard <andrew@andrewrynhard.com>
2019-08-25 18:44:37 +0000

    feat: add sequencer interface
    This adds an interface that can be used to descibe boot, shutdown, and
    upgrade events in a set of phases.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

---
+++ internal/app/machined/internal/sequencer/sequencer.go
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package sequencer
+
+import (
+	"github.com/talos-systems/talos/internal/app/machined/internal/sequencer/v1alpha1"
+)
+
+// Sequencer describes the boot, shutdown, and upgrade events.
+type Sequencer interface {
+	Boot() error
+	Shutdown() error
+	Upgrade() error
+}
+
+// Version represents the sequencer version.
+type Version int
+
+const (
+	// V1Alpha1 is the v1alpha1 sequencer.
+	V1Alpha1 = iota
+)
+
+// New initializes and returns a sequencer based on the specified version.
+func New(v Version) Sequencer {
+	switch v {
+	case V1Alpha1:
+		return &v1alpha1.Sequencer{}
+	default:
+		return nil
+	}
+}
--- internal/app/machined/main.go
+++ internal/app/machined/main.go
@@ -13,119 +13,25 @@ import (
 	"golang.org/x/sys/unix"
 
 	"github.com/talos-systems/talos/internal/app/machined/internal/event"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/acpi"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/network"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/platform"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/rootfs"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/security"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/services"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/signal"
-	"github.com/talos-systems/talos/internal/app/machined/internal/phase/sysctls"
-	userdatatask "github.com/talos-systems/talos/internal/app/machined/internal/phase/userdata"
-	"github.com/talos-systems/talos/internal/app/machined/pkg/system"
+	"github.com/talos-systems/talos/internal/app/machined/internal/sequencer"
 	"github.com/talos-systems/talos/pkg/constants"
 	"github.com/talos-systems/talos/pkg/startup"
-	"github.com/talos-systems/talos/pkg/userdata"
 )
-
-func run() (err error) {
-	if err = startup.RandSeed(); err != nil {
-		return err
-	}
-
-	if err = os.Setenv("PATH", constants.PATH); err != nil {
-		return errors.New("error setting PATH")
-	}
-
-	data := &userdata.UserData{}
-	phaserunner, err := phase.NewRunner(data)
-	if err != nil {
-		return err
-	}
-
-	phaserunner.Add(
-		phase.NewPhase(
-			"system requirements",
-			security.NewSecurityTask(),
-			rootfs.NewSystemDirectoryTask(),
-			rootfs.NewMountBPFFSTask(),
-			rootfs.NewMountCgroupsTask(),
-			rootfs.NewMountSubDevicesTask(),
-			sysctls.NewSysctlsTask(),
-		),
-		phase.NewPhase(
-			"basic system configuration",
-			rootfs.NewNetworkConfigurationTask(),
-			rootfs.NewOSReleaseTask(),
-		),
-		// Break out network setup into a separate phase
-		// so we can use the well known resolv.conf location
-		// /etc/resolv.conf
-		phase.NewPhase(
-			"initial network",
-			network.NewUserDefinedNetworkTask(),
-		),
-		phase.NewPhase(
-			"userdata",
-			userdatatask.NewUserDataTask(),
-		),
-		phase.NewPhase(
-			"mount extra devices",
-			userdatatask.NewExtraDevicesTask(),
-		),
-		phase.NewPhase(
-			"user requests",
-			userdatatask.NewPKITask(),
-			userdatatask.NewExtraEnvVarsTask(),
-			userdatatask.NewExtraFilesTask(),
-		),
-		phase.NewPhase(
-			"platform tasks",
-			platform.NewPlatformTask(),
-		),
-		phase.NewPhase(
-			"installation verification",
-			rootfs.NewCheckInstallTask(),
-		),
-		phase.NewPhase(
-			"overlay mounts",
-			rootfs.NewMountOverlayTask(),
-			rootfs.NewMountSharedTask(),
-		),
-		phase.NewPhase(
-			"setup /var",
-			rootfs.NewVarDirectoriesTask(),
-		),
-		phase.NewPhase(
-			"save userdata",
-			userdatatask.NewSaveUserDataTask(),
-		),
-		phase.NewPhase(
-			"service setup",
-			acpi.NewHandlerTask(),
-			services.NewServicesTask(),
-			signal.NewHandlerTask(),
-		),
-	)
-
-	if err = phaserunner.Run(); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func recovery() {
 	if r := recover(); r != nil {
-		log.Printf("recovered from: %+v\n", r)
+		log.Printf("%+v\n", r)
 		for i := 10; i >= 0; i-- {
 			log.Printf("rebooting in %d seconds\n", i)
 			time.Sleep(1 * time.Second)
 		}
+		if unix.Reboot(unix.LINUX_REBOOT_CMD_RESTART) == nil {
+			select {}
+		}
 	}
 }
 
+// See http://man7.org/linux/man-pages/man2/reboot.2.html.
 func sync() {
 	syncdone := make(chan struct{})
 
@@ -151,55 +57,68 @@ func sync() {
 	log.Printf("sync hasn't completed in time, aborting...")
 }
 
-var rebootFlag = unix.LINUX_REBOOT_CMD_RESTART
-
-func reboot() {
-	// See http://man7.org/linux/man-pages/man2/reboot.2.html.
-	sync()
-
-	if unix.Reboot(rebootFlag) == nil {
-		select {}
-	}
-}
-
+// nolint: gocyclo
 func main() {
-	// This is main entrypoint into machined execution, control is passed here from init after switch root.
+	var err error
+
+	// This is main entrypoint into machined execution, control is passed here
+	// from init after switch root.
 	//
-	// When machined terminates either on normal shutdown (reboot, poweroff), or due to panic, control
-	// goes through recovery() and reboot() functions below, which finalize node state - sync buffers,
-	// initiate poweroff or reboot. Also on shutdown, other deferred function are called, for example
+	// When machined terminates either on normal shutdown (reboot, poweroff), or
+	// due to panic, control goes through recovery() and reboot() functions
+	// below, which finalize node state - sync buffers, initiate poweroff or
+	// reboot. Also on shutdown, other deferred function are called, for example
 	// services are gracefully shutdown.
 
-	defer reboot()
-
-	// on any return from init.main(), initiate host reboot or shutdown
-	// handle any panics in the main goroutine, and proceed to reboot() above
+	// On any return from init.main(), initiate host reboot or shutdown handle
+	// any panics in the main goroutine, and proceed to reboot() above
 	defer recovery()
 
-	// subscribe for events
+	// Subscribe to events.
 	events := make(chan event.Type, 5) // provide some buffer to avoid blocking the bus
 	event.Bus().Subscribe(events)
 	defer event.Bus().Unsubscribe(events)
 
-	// run startup phases
-	if err := run(); err != nil {
+	// Ensure rng is seeded.
+	if err = startup.RandSeed(); err != nil {
+		panic(err)
+	}
+
+	// Set the PATH env var.
+	if err = os.Setenv("PATH", constants.PATH); err != nil {
+		panic(errors.New("error setting PATH"))
+	}
+
+	// Boot the machine.
+	seq := sequencer.New(sequencer.V1Alpha1)
+
+	if err := seq.Boot(); err != nil {
 		panic(errors.Wrap(err, "boot failed"))
 	}
 
-	// start services
-	system.Services(nil).StartAll()
-	defer system.Services(nil).Shutdown()
+	var rebootFlag = unix.LINUX_REBOOT_CMD_RESTART
 
-	// wait for events
+	// Wait for an event.
+
 	for {
 		switch <-events {
-		case event.Reboot:
-			return
 		case event.Shutdown:
 			rebootFlag = unix.LINUX_REBOOT_CMD_POWER_OFF
-			return
+			fallthrough
+		case event.Reboot:
+			if err := seq.Shutdown(); err != nil {
+				panic(errors.Wrap(err, "shutdown failed"))
+			}
+
+			sync()
+
+			if unix.Reboot(rebootFlag) == nil {
+				select {}
+			}
 		case event.Upgrade:
-			// TODO: not implemented yet
+			if err := seq.Upgrade(); err != nil {
+				panic(errors.Wrap(err, "upgrade failed"))
+			}
 		}
 	}
 }
