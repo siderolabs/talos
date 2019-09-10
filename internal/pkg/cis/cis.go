2c4f3a68060b0048c30ad72a16f143a200a119b9
Andrew Rynhard <andrew@andrewrynhard.com>
2019-09-10 16:05:54 +0000

    chore: remove invalid TODO
    This TODO no longer applies. We have setteled on a fixed boot size. This
    also removes variables no longer needed.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- internal/pkg/cis/cis.go
+++ internal/pkg/cis/cis.go
@@ -40,8 +40,6 @@ resources:
 `
 
 // EnforceAuditingRequirements enforces CIS requirements for auditing.
-// TODO(andrewrynhard): Enable audit-log-maxbackup.
-// TODO(andrewrynhard): Enable audit-log-maxsize.
 func EnforceAuditingRequirements(cfg *kubeadmapi.ClusterConfiguration) error {
 	if err := ioutil.WriteFile("/etc/kubernetes/audit-policy.yaml", []byte(auditPolicy), 0400); err != nil {
 		return err
