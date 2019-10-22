98ae71de5d9bbd3d0545762c5586917d980ab686
Andrew Rynhard <andrew@andrewrynhard.com>
2019-10-22 01:35:23 +0000

    docs: improve asciinema casts
    This replaces the existing casts with longer waits so that users can
    parse the output.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- docs/website/components/Terminal.vue
+++ docs/website/components/Terminal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="terminal w-1/2 mx-auto">
+  <div class="terminal mx-auto">
     <div id="terminal-body">
       <div id="terminal-player-wrapper"></div>
     </div>
@@ -10,7 +10,7 @@
           :key="cast.src"
           class="bg-primary-color-500 text-white font-semibold m-1 p-1 rounded"
           style="width: 100px"
-          @click="handleClick(cast.src)"
+          @click="handleClick(cast.src, cast.cols, cast.rows)"
         >
           <span class="mr-1">{{ cast.title }}</span>
         </button>
@@ -26,35 +26,50 @@ export default {
   data() {
     return {
       casts: [
-        { title: 'Cluster', src: '/cluster-create.cast' },
-        { title: 'Services', src: '/osctl-services.cast' },
-        { title: 'Routes', src: '/osctl-routes.cast' },
-        { title: 'Interfaces', src: '/osctl-interfaces.cast' },
-        { title: 'Containers', src: '/osctl-containers.cast' },
-        { title: 'Processes', src: '/osctl-processes.cast' },
-        { title: 'Mounts', src: '/osctl-mounts.cast' }
+        { title: 'Cluster', src: '/cluster-create.cast', cols: 109, rows: 29 },
+        { title: 'Services', src: '/osctl-services.cast', cols: 109, rows: 29 },
+        { title: 'Routes', src: '/osctl-routes.cast', cols: 109, rows: 29 },
+        {
+          title: 'Interfaces',
+          src: '/osctl-interfaces.cast',
+          cols: 109,
+          rows: 29
+        },
+        {
+          title: 'Containers',
+          src: '/osctl-containers.cast',
+          cols: 109,
+          rows: 29
+        },
+        {
+          title: 'Processes',
+          src: '/osctl-processes.cast',
+          cols: 109,
+          rows: 29
+        },
+        { title: 'Mounts', src: '/osctl-mounts.cast', cols: 109, rows: 29 }
       ]
     }
   },
 
   mounted() {
-    this.handleClick('/cluster-create.cast')
+    const cast = this.casts[0]
+    this.handleClick(cast.src, cast.cols, cast.rows)
   },
 
   methods: {
-    handleClick(src) {
+    handleClick(src, cols, rows) {
       const terminalPlayerWrapper = document.getElementById(
         'terminal-player-wrapper'
       )
-      const terminalRows = 25
       terminalPlayerWrapper.innerHTML =
-        '<asciinema-player id="terminal-player" cols="100" rows="' +
-        terminalRows +
-        '" preload autoplay loop speed="1.0" src="' +
+        '<asciinema-player id="terminal-player" cols="' +
+        cols +
+        '"rows="' +
+        rows +
+        '" preload autoplay loop speed="1.0" font-size="small" src="' +
         src +
         '"></asciinema-player>'
-      console.log(src)
-      this.src = src
     }
   }
 }
