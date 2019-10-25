dd1e830055b5a40611a9ed5e0a732ff2040169f1
Andrey Smirnov <smirnov.andrey@gmail.com>
2019-10-25 23:45:41 +0300

    chore: replace `/* */` comments with `//` comments in license header
    This fixes issues with `// +build` directives not being recognized in
    source files.
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- internal/pkg/event/event.go
+++ internal/pkg/event/event.go
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // Package event implements an embeddable type that uses the observation
 // pattern to facilitate an event bus.
