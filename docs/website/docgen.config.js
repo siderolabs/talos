b02051d8eb66c0d8d67aa2757f5be27fb20aac4e
Andrew Rynhard <andrew@andrewrynhard.com>
2019-10-30 22:23:51 +0000

    docs: add troubleshooting guide on common PKI scenarios
    This adds a "Troubleshooting" section to the documention along with a
    guide on generating a certificate. This covers the scenario when a
    user's certificate has expired.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>

--- docs/website/docgen.config.js
+++ docs/website/docgen.config.js
@@ -3,6 +3,6 @@ module.exports = {
   availableRoutesFile: `${__dirname}/routes.json`,
   contentInputFolder: `${__dirname}/content/`,
   menuInputFile: `${__dirname}/content/{version}.{lang}.json`,
-  sectionsOutputFile: `${__dirname}/static/{version}.sections.{lang}.json`,
-  menuOutputFile: `${__dirname}/static/{version}.menu.{lang}.json`
+  menuOutputFile: `${__dirname}/static/{version}.menu.{lang}.json`,
+  sectionsOutputFile: `${__dirname}/static/{version}.sections.{lang}.json`
 }
