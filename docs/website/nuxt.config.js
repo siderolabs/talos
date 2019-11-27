83d9e0121792f39d28482d0cba3bcc78c8dee409
Tim Gerla <tim@gerla.net>
2019-11-27 10:41:22 -0800

    feat: add Google Analytics tracking to the project website
    - add the Google Analytics tracking package and tracking ID.
    
    Signed-off-by: Tim Gerla <tim@gerla.net>

--- docs/website/nuxt.config.js
+++ docs/website/nuxt.config.js
@@ -43,7 +43,10 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-141692582-2'
+    }],
   ],
 
   /*
