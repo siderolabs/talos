ed8b9d07d752d881f185b7bdf4b6b659edd6661d
Andrey Smirnov <smirnov.andrey@gmail.com>
2019-12-18 00:55:23 +0300

    fix: fail on muliple nodes for commands which don't support it
    Fixes #1663
    
    (I believe it's 0.3 backport strong candidate).
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>
    (cherry picked from commit f3dff87957fa8e0a47c4cd05dd99e0fad3dd8287)

---
+++ internal/integration/cli/list.go
@@ -0,0 +1,33 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// +build integration_cli
+
+package cli
+
+import (
+	"regexp"
+
+	"github.com/talos-systems/talos/internal/integration/base"
+)
+
+// ListSuite verifies dmesg command
+type ListSuite struct {
+	base.CLISuite
+}
+
+// SuiteName ...
+func (suite *ListSuite) SuiteName() string {
+	return "cli.ListSuite"
+}
+
+// TestSuccess runs comand with success.
+func (suite *ListSuite) TestSuccess() {
+	suite.RunOsctl([]string{"list", "/etc"},
+		base.StdoutShouldMatch(regexp.MustCompile(`os-release`)))
+}
+
+func init() {
+	allSuites = append(allSuites, new(ListSuite))
+}
