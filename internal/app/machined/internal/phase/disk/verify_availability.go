72b84bb7c57d4c9093ae75af42791a4b2c4a858f
Andrew Rynhard <andrew@andrewrynhard.com>
2020-04-22 11:19:00 -0700

    fix: ensure disk is not busy
    This fixes a few bugs that collectively caused `EBUSY` error when trying
    to format the system disk. It ensures that we use the correct path to
    the partition, and requires the path to exist. It also removes the
    `/etc/kubernetes` mount from `trustd` as it was discovered that this
    mount was causing the `EBUSY`.
    
    Signed-off-by: Andrew Rynhard <andrew@andrewrynhard.com>
    (cherry picked from commit d91628ac3d9c98062f760b308d8344c26a6fe1ad)

--- internal/app/machined/internal/phase/disk/verify_availability.go
+++ internal/app/machined/internal/phase/disk/verify_availability.go
@@ -6,6 +6,7 @@ package disk
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -35,36 +36,50 @@ func NewVerifyDiskAvailabilityTask(devname string) phase.Task {
 // TaskFunc returns the runtime function.
 func (task *VerifyDiskAvailability) TaskFunc(mode runtime.Mode) phase.TaskFunc {
 	return func(r runtime.Runtime) error {
-		return task.standard()
+		//  We only need to verify system disk availability if we are going to
+		// reformat the ephemeral partition.
+		if r.Config().Machine().Install().Force() {
+			return task.standard()
+		}
+
+		return nil
 	}
 }
 
 func (task *VerifyDiskAvailability) standard() (err error) {
+	if _, err = os.Stat(task.devname); errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("system disk not found: %w", err)
+	}
+
 	mountsReported := false
 
 	return retry.Constant(3*time.Minute, retry.WithUnits(500*time.Millisecond)).Retry(func() error {
-		if isBusy(task.devname) {
-			if !mountsReported {
-				// if disk is busy, report mounts for debugging purposes but just once
-				// otherwise console might be flooded with messages
-				dumpMounts()
-				mountsReported = true
+		if err = tryLock(task.devname); err != nil {
+			if err == unix.EBUSY {
+				if !mountsReported {
+					// if disk is busy, report mounts for debugging purposes but just once
+					// otherwise console might be flooded with messages
+					dumpMounts()
+					mountsReported = true
+				}
+
+				return retry.ExpectedError(errors.New("system disk in use"))
 			}
 
-			return retry.ExpectedError(errors.New("system disk in use"))
+			return retry.UnexpectedError(fmt.Errorf("failed to verify system disk not in use: %w", err))
 		}
 
 		return nil
 	})
 }
 
-func isBusy(devname string) bool {
+func tryLock(devname string) error {
 	fd, errno := unix.Open(devname, unix.O_RDONLY|unix.O_EXCL|unix.O_CLOEXEC, 0)
 
 	// nolint: errcheck
 	defer unix.Close(fd)
 
-	return errno == unix.EBUSY
+	return errno
 }
 
 func dumpMounts() {
