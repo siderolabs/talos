f8ff98a27b952f4a317ff83fb3f59c0a3476b796
Spencer Smith <robertspencersmith@gmail.com>
2020-04-29 15:24:07 -0400

    chore: use clusterctl and v1alpha3 providers for tests
    This PR will update our testing ocde to make use of the clusterctl tool,
    as well as use the newer versions of various providers and updated
    manifests.
    
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- hack/test/e2e-capi.sh
+++ hack/test/e2e-capi.sh
@@ -4,29 +4,36 @@ set -eou pipefail
 
 source ./hack/test/e2e.sh
 
+export CABPT_VERSION="0.2.0-alpha.0"
+export CAPA_VERSION="0.5.2"
+
 # We need to override this here since e2e.sh will set it to ${TMP}/capi/kubeconfig.
 export KUBECONFIG="/tmp/e2e/docker/kubeconfig"
 
-# CAPI
-
-export CAPI_VERSION="0.2.9"
-export CAPI_COMPONENTS="https://github.com/kubernetes-sigs/cluster-api/releases/download/v${CAPI_VERSION}/cluster-api-components.yaml"
-
 # CABPT
-
 export CABPT_NS="cabpt-system"
 
 # Install envsubst
 apk add --no-cache gettext
 
-export AWS_B64ENCODED_CREDENTIALS=${AWS_SVC_ACCT}
-cat ${PWD}/hack/test/capi/components-capa.yaml| envsubst | ${KUBECTL} apply -f -
-
+# Env vars for cloud accounts
 export GCP_B64ENCODED_CREDENTIALS=${GCE_SVC_ACCT}
-cat ${PWD}/hack/test/capi/components-capg.yaml| envsubst | ${KUBECTL} apply -f -
+export AWS_B64ENCODED_CREDENTIALS=${AWS_SVC_ACCT}
 
-cat ${PWD}/hack/test/capi/components-provider.yaml | ${KUBECTL} apply -f -
-${KUBECTL} apply -f ${CAPI_COMPONENTS}
+cat << EOF > /tmp/e2e/clusterctl.yaml
+providers:
+  - name: "talos"
+    url: "https://github.com/talos-systems/cluster-api-bootstrap-provider-talos/releases/latest/bootstrap-components.yaml"
+    type: "BootstrapProvider"
+EOF
+
+${CLUSTERCTL} init \
+    --config /tmp/e2e/clusterctl.yaml \
+    --control-plane "-" \
+    --infrastructure "aws:v${CAPA_VERSION}" \
+    --bootstrap "talos:v${CABPT_VERSION}"
+
+cat ${PWD}/hack/test/capi/components-capg.yaml| envsubst | ${KUBECTL} apply -f -
 
 # Wait for the talosconfig
 timeout=$(($(date +%s) + ${TIMEOUT}))
