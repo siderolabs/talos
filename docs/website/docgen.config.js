8ca862ee326849fb5ad2998bd48e762d35b25676
Timothy Gerla <tim@gerla.net>
2020-05-13 08:33:20 -0400

    docs: add a sitemap and Netlify redirects
    - add nuxtjs/sitemap for an automatic sitemap generator
    - add auto-generated explicit redirects for docs pages: right now, if you
    navigate to a deep docs page (/docs/v0.5/en/guides/cloud/aws, for instance),
    you will get an HTTP 404 from Netlify because the page doesn't exist
    on disk, but the resulting single-page-app javascript will show you the content.
    These redirects are an attempt to solve the 404 problem which probably affects
    search engines.
    
    Signed-off-by: Timothy Gerla <tim@gerla.net>

--- docs/website/docgen.config.js
+++ docs/website/docgen.config.js
@@ -4,5 +4,6 @@ module.exports = {
   contentInputFolder: `${__dirname}/content/`,
   menuInputFile: `${__dirname}/content/{version}.{lang}.json`,
   menuOutputFile: `${__dirname}/static/{version}.menu.{lang}.json`,
-  sectionsOutputFile: `${__dirname}/static/{version}.sections.{lang}.json`
+  sectionsOutputFile: `${__dirname}/static/{version}.sections.{lang}.json`,
+  redirectsOutputFile: `${__dirname}/static/_redirects`
 }
