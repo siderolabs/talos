0617a10027743beac7a1e4cd3c3322d6c05bcab8
Andrew Rynhard <andrew@rynhard.io>
2020-07-14 09:55:08 -0700

    feat: upgrade Kubernetes to v1.19.0-rc.0
    This brings in the latest version of Kubernetes.
    
    Signed-off-by: Andrew Rynhard <andrew@rynhard.io>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -139,7 +139,7 @@ const (
 	KubeletBootstrapKubeconfig = "/etc/kubernetes/bootstrap-kubeconfig"
 
 	// DefaultKubernetesVersion is the default target version of the control plane.
-	DefaultKubernetesVersion = "1.19.0-beta.1"
+	DefaultKubernetesVersion = "1.19.0-rc.0"
 
 	// KubeletImage is the enforced kubelet image to use.
 	KubeletImage = "docker.io/autonomy/kubelet"
