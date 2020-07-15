59af6d61a6c136aab544b797213ac21a49a6314a
Spencer Smith <robertspencersmith@gmail.com>
2020-07-15 19:00:28 -0400

    chore: update capi CI manifests to use control planes
    This PR will update the CI testing to make use of our control plane
    provider, as well as the other CAPI components.
    
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- hack/test/e2e.sh
+++ hack/test/e2e.sh
@@ -49,26 +49,45 @@ function create_cluster_capi {
 
   ${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig apply -f ${TMP}/cluster.yaml
 
+  # Wait for first controlplane machine to have a name
+  timeout=$(($(date +%s) + ${TIMEOUT}))
+  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -l cluster.x-k8s.io/control-plane,cluster.x-k8s.io/cluster-name=${NAME_PREFIX} --all-namespaces -o json | jq -re '.items[0].metadata.name | select (.!=null)')" ]; do
+    [[ $(date +%s) -gt $timeout ]] && exit 1
+    sleep 10
+    ${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -l cluster.x-k8s.io/control-plane,cluster.x-k8s.io/cluster-name=${NAME_PREFIX} --all-namespaces
+  done
+
+  FIRST_CP_NODE=$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -l cluster.x-k8s.io/control-plane,cluster.x-k8s.io/cluster-name=${NAME_PREFIX} --all-namespaces -o json | jq -r '.items[0].metadata.name')
+
+  # Wait for first controlplane machine to have a talosconfig ref
+  timeout=$(($(date +%s) + ${TIMEOUT}))
+  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine ${FIRST_CP_NODE} -o json | jq -re '.spec.bootstrap.configRef.name | select (.!=null)')" ]; do
+    [[ $(date +%s) -gt $timeout ]] && exit 1
+    sleep 10
+  done
+
+  FIRST_CP_TALOSCONFIG=$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine ${FIRST_CP_NODE} -o json | jq -re '.spec.bootstrap.configRef.name')
+
   # Wait for talosconfig in cm then dump it out
   timeout=$(($(date +%s) + ${TIMEOUT}))
-  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get talosconfig ${NAME_PREFIX}-controlplane-0 -o jsonpath='{.status.talosConfig}')" ]; do
+  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get talosconfig ${FIRST_CP_TALOSCONFIG} -o jsonpath='{.status.talosConfig}')" ]; do
     [[ $(date +%s) -gt $timeout ]] && exit 1
     sleep 10
   done
-  ${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get talosconfig ${NAME_PREFIX}-controlplane-0 -o jsonpath='{.status.talosConfig}' > ${TALOSCONFIG}
+  ${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get talosconfig ${FIRST_CP_TALOSCONFIG} -o jsonpath='{.status.talosConfig}' > ${TALOSCONFIG}
 
-  # Wait until we have an IP for master 0
+  # Wait until we have an IP for first controlplane node
   timeout=$(($(date +%s) + ${TIMEOUT}))
-  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -o go-template --template='{{range .status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}}' ${NAME_PREFIX}-controlplane-0)" ]; do
+  until [ -n "$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -o go-template --template='{{range .status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}}' ${FIRST_CP_NODE})" ]; do
     [[ $(date +%s) -gt $timeout ]] && exit 1
     sleep 10
   done
 
-  MASTER_IP=$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -o go-template --template='{{range .status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}}' ${NAME_PREFIX}-controlplane-0)
+  MASTER_IP=$(${KUBECTL} --kubeconfig /tmp/e2e/docker/kubeconfig get machine -o go-template --template='{{range .status.addresses}}{{if eq .type "ExternalIP"}}{{.address}}{{end}}{{end}}' ${FIRST_CP_NODE})
   "${TALOSCTL}" config endpoint "${MASTER_IP}"
   "${TALOSCTL}" config node "${MASTER_IP}"
 
-  # Wait for the kubeconfig from capi master-0
+  # Wait for the kubeconfig from first cp node
   timeout=$(($(date +%s) + ${TIMEOUT}))
   until get_kubeconfig; do
     [[ $(date +%s) -gt $timeout ]] && exit 1
