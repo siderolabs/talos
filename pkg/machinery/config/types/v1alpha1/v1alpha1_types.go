25a65efb2ea876ca8096912e5860c63c2cb1bbe1
Spencer Smith <robertspencersmith@gmail.com>
2020-09-25 10:23:30 -0400

    docs: add note around link-local addressing
    This PR adds a small note to the config docs to detail how to do link
    local networking like SLAAC.
    
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- pkg/machinery/config/types/v1alpha1/v1alpha1_types.go
+++ pkg/machinery/config/types/v1alpha1/v1alpha1_types.go
@@ -443,6 +443,10 @@ type NetworkConfig struct {
 	//
 	//     > Note: This option is mutually exclusive with CIDR.
 	//
+	//     > Note: To configure an interface with *only* IPv6 SLAAC addressing, CIDR should be set to "" and DHCP to false
+	//     > in order for Talos to skip configuration of addresses.
+	//     > All other options will still apply.
+	//
 	//     ##### machine.network.interfaces.ignore
 	//
 	//     `ignore` is used to exclude a specific interface from configuration.
