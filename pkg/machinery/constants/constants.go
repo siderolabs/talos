47dbf4996f9c08c07001b8961614b349ed6fae36
Andrey Smirnov <smirnov.andrey@gmail.com>
2020-10-16 17:02:28 +0300

    feat: bump packages for Linux 5.8.15 and containerd 1.4.1
    See:
    
    * https://github.com/talos-systems/pkgs/pull/179
    * https://github.com/talos-systems/pkgs/pull/178
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version
-	DefaultKernelVersion = "5.8.10-talos"
+	DefaultKernelVersion = "5.8.15-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL
 	// to the config.
@@ -235,7 +235,7 @@ const (
 	TrustdPort = 50001
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "1.4.0"
+	DefaultContainerdVersion = "1.4.1"
 
 	// SystemContainerdNamespace is the Containerd namespace for Talos services.
 	SystemContainerdNamespace = "system"
