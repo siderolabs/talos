c1c2fa3bf0b4c6b53fe90a90bc93a1837e2552b8
Andrew Rynhard <andrew@rynhard.io>
2020-11-26 06:52:29 -0800

    feat: update Containerd and Linux
    This brings in Containerd v1.4.2 and Linux v5.9.11
    
    Signed-off-by: Andrew Rynhard <andrew@rynhard.io>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "5.9.3-talos"
+	DefaultKernelVersion = "5.9.11-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
@@ -272,7 +272,7 @@ const (
 	TrustdPort = 50001
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "1.4.1"
+	DefaultContainerdVersion = "1.4.2"
 
 	// SystemContainerdNamespace is the Containerd namespace for Talos services.
 	SystemContainerdNamespace = "system"
