621968977ee336b81490512b026451d945d60ec5
Andrey Smirnov <smirnov.andrey@gmail.com>
2020-12-01 19:03:36 +0300

    feat: update kubernetes to 1.20.0-rc.0
    Talos 0.8 is going to ship with K8s 1.20.
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -182,7 +182,7 @@ const (
 	KubeletBootstrapKubeconfig = "/etc/kubernetes/bootstrap-kubeconfig"
 
 	// DefaultKubernetesVersion is the default target version of the control plane.
-	DefaultKubernetesVersion = "1.20.0-beta.2"
+	DefaultKubernetesVersion = "1.20.0-rc.0"
 
 	// DefaultControlPlanePort is the default port to use for the control plane.
 	DefaultControlPlanePort = 6443
