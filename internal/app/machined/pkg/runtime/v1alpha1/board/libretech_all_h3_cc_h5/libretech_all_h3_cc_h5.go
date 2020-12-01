88f15b12547d1d3af36dc622f303f1c121af3133
Andrew Rynhard <andrew@rynhard.io>
2020-11-30 17:09:02 -0800

    fix: use the dtb from kernel pkg for libretech_all_h3_cc_h5
    This adds sun50i-h5-libretech-all-h3-cc.dtb to the EFI partition.
    
    Signed-off-by: Andrew Rynhard <andrew@rynhard.io>

--- internal/app/machined/pkg/runtime/v1alpha1/board/libretech_all_h3_cc_h5/libretech_all_h3_cc_h5.go
+++ internal/app/machined/pkg/runtime/v1alpha1/board/libretech_all_h3_cc_h5/libretech_all_h3_cc_h5.go
@@ -9,16 +9,19 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/sys/unix"
 
 	"github.com/talos-systems/talos/internal/app/machined/pkg/runtime"
+	"github.com/talos-systems/talos/pkg/copy"
 	"github.com/talos-systems/talos/pkg/machinery/constants"
 )
 
 var (
 	bin       = fmt.Sprintf("/usr/install/u-boot/%s/u-boot-sunxi-with-spl.bin", constants.BoardLibretechAllH3CCH5)
 	off int64 = 1024 * 8
+	dtb       = "/dtb/allwinner/sun50i-h5-libretech-all-h3-cc.dtb"
 )
 
 // LibretechAllH3CCH5 represents the Libre Computer ALL-H3-CC (Tritium).
@@ -63,6 +66,19 @@ func (l *LibretechAllH3CCH5) Install(disk string) (err error) {
 	// to esure that the file is written before the loopback device is
 	// unmounted.
 	err = f.Sync()
+	if err != nil {
+		return err
+	}
+
+	src := "/usr/install" + dtb
+	dst := "/boot/EFI" + dtb
+
+	err = os.MkdirAll(filepath.Dir(dst), 0o600)
+	if err != nil {
+		return err
+	}
+
+	err = copy.File(src, dst)
 	if err != nil {
 		return err
 	}
