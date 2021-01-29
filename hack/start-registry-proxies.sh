32333457c23f8190d1ad25eb5e1efd6d052ceebe
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-01-29 19:58:29 +0300

    chore: add script for starting registry proxies
    To avoid copying and pasting it from the documentation every time.
    
    Signed-off-by: Alexey Palazhchenko <alexey.palazhchenko@gmail.com>

---
+++ hack/start-registry-proxies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Sync changes with configuring-pull-through-cache.md.
+
+set -e
+
+docker run -d -p 5000:5000 \
+    -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
+    --restart always \
+    --name registry-docker.io registry:2
+
+docker run -d -p 5001:5000 \
+    -e REGISTRY_PROXY_REMOTEURL=https://k8s.gcr.io \
+    --restart always \
+    --name registry-k8s.gcr.io registry:2
+
+docker run -d -p 5002:5000 \
+    -e REGISTRY_PROXY_REMOTEURL=https://quay.io \
+    --restart always \
+    --name registry-quay.io registry:2.5
+
+docker run -d -p 5003:5000 \
+    -e REGISTRY_PROXY_REMOTEURL=https://gcr.io \
+    --restart always \
+    --name registry-gcr.io registry:2
+
+docker run -d -p 5004:5000 \
+    -e REGISTRY_PROXY_REMOTEURL=https://ghcr.io \
+    --restart always \
+    --name registry-ghcr.io registry:2
