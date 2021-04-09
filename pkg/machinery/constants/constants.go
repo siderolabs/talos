e3585f24bcaebf2f00f2b31878927eede6c5b000
Andrey Smirnov <smirnov.andrey@gmail.com>
2021-04-09 23:29:35 +0300

    chore: update Linux to 5.10.28, u-boot to final 2021.04 release
    See talos-systems/pkgs#261 talos-systems/pkgs#262
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "5.10.27-talos"
+	DefaultKernelVersion = "5.10.28-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
