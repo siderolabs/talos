04ddda962fbcfdeaae59d232e7bb7f9c5bb63bc7
Andrey Smirnov <smirnov.andrey@gmail.com>
2021-05-20 00:25:52 +0300

    feat: update containerd to 1.5.2, runc to 1.0.0-rc95
    This also updates libseccomp and add support for `netxen` networkd card.
    
    This addresses[CVE-2021-30465](https://github.com/opencontainers/runc/security/advisories/GHSA-c3xm-pvg7-gh7r).
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -294,7 +294,7 @@ const (
 	TrustdPort = 50001
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "1.5.1"
+	DefaultContainerdVersion = "1.5.2"
 
 	// SystemContainerdNamespace is the Containerd namespace for Talos services.
 	SystemContainerdNamespace = "system"
