79b8fa64b9453917860faae3df5d14647186b9ba
Andrey Smirnov <smirnov.andrey@gmail.com>
2021-08-03 19:04:42 +0300

    feat: update containerd to 1.5.5
    * https://github.com/containerd/containerd/releases/tag/v1.5.5
    
    Signed-off-by: Andrey Smirnov <smirnov.andrey@gmail.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -312,7 +312,7 @@ const (
 	TrustdPort = 50001
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "1.5.4"
+	DefaultContainerdVersion = "1.5.5"
 
 	// SystemContainerdNamespace is the Containerd namespace for Talos services.
 	SystemContainerdNamespace = "system"
