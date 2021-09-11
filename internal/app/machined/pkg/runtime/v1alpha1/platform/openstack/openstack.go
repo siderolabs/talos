3de505c894274bfd5248b6c597f6e3a53f873ba1
Serge Logvinov <serge.logvinov@sinextra.dev>
2021-09-11 11:12:07 +0000

    fix: skip bad cloud-config in OpenStack platform
    Sometimes we cannot redefine user-data in the openstack.
    we need to catch this and allow user to apply configuration
    through api.
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- internal/app/machined/pkg/runtime/v1alpha1/platform/openstack/openstack.go
+++ internal/app/machined/pkg/runtime/v1alpha1/platform/openstack/openstack.go
@@ -5,12 +5,10 @@
 package openstack
 
 import (
+	"bytes"
 	"context"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
-	"net/http"
 
 	"github.com/talos-systems/go-procfs/procfs"
 
@@ -34,82 +32,73 @@ const (
 type Openstack struct{}
 
 // Name implements the runtime.Platform interface.
-func (a *Openstack) Name() string {
+func (o *Openstack) Name() string {
 	return "openstack"
 }
 
 // Configuration implements the runtime.Platform interface.
-func (a *Openstack) Configuration(ctx context.Context) ([]byte, error) {
+func (o *Openstack) Configuration(ctx context.Context) ([]byte, error) {
 	log.Printf("fetching machine config from: %q", OpenstackUserDataEndpoint)
 
-	return download.Download(ctx, OpenstackUserDataEndpoint,
+	machineConfigDl, err := download.Download(ctx, OpenstackUserDataEndpoint,
 		download.WithErrorOnNotFound(errors.ErrNoConfigSource),
 		download.WithErrorOnEmptyResponse(errors.ErrNoConfigSource))
+	if err != nil {
+		return nil, err
+	}
+
+	// Some openstack setups does not allow you to change user-data,
+	// so skip this case.
+	if bytes.HasPrefix(machineConfigDl, []byte("#cloud-config")) {
+		return nil, errors.ErrNoConfigSource
+	}
+
+	return machineConfigDl, nil
 }
 
 // Mode implements the runtime.Platform interface.
-func (a *Openstack) Mode() runtime.Mode {
+func (o *Openstack) Mode() runtime.Mode {
 	return runtime.ModeCloud
 }
 
 // Hostname implements the runtime.Platform interface.
-func (a *Openstack) Hostname(ctx context.Context) (hostname []byte, err error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, OpenstackHostnameEndpoint, nil)
+func (o *Openstack) Hostname(ctx context.Context) (hostname []byte, err error) {
+	log.Printf("fetching hostname from: %q", OpenstackHostnameEndpoint)
+
+	hostname, err = download.Download(ctx, OpenstackHostnameEndpoint,
+		download.WithErrorOnNotFound(errors.ErrNoHostname),
+		download.WithErrorOnEmptyResponse(errors.ErrNoHostname))
 	if err != nil {
-		return nil, err
+		// Platform cannot support this endpoint, or return timeout.
+		// ApplyDynamicConfig can crash in this situation.
+		log.Printf("failed to fetch hostname, ignored: %s", err)
+
+		return nil, nil
 	}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	//nolint:errcheck
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return hostname, fmt.Errorf("failed to fetch hostname from metadata service: %d", resp.StatusCode)
-	}
-
-	return ioutil.ReadAll(resp.Body)
+	return hostname, nil
 }
 
 // ExternalIPs implements the runtime.Platform interface.
-func (a *Openstack) ExternalIPs(ctx context.Context) (addrs []net.IP, err error) {
-	var (
-		body []byte
-		req  *http.Request
-		resp *http.Response
-	)
+func (o *Openstack) ExternalIPs(ctx context.Context) (addrs []net.IP, err error) {
+	log.Printf("fetching externalIP from: %q", OpenstackExternalIPEndpoint)
 
-	if req, err = http.NewRequestWithContext(ctx, "GET", OpenstackExternalIPEndpoint, nil); err != nil {
-		return
+	exIP, err := download.Download(ctx, OpenstackExternalIPEndpoint,
+		download.WithErrorOnNotFound(errors.ErrNoExternalIPs),
+		download.WithErrorOnEmptyResponse(errors.ErrNoExternalIPs))
+	if err != nil {
+		return nil, err
 	}
 
-	client := &http.Client{}
-	if resp, err = client.Do(req); err != nil {
-		return
-	}
-
-	//nolint:errcheck
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return addrs, fmt.Errorf("failed to retrieve external addresses for instance")
-	}
-
-	if body, err = ioutil.ReadAll(resp.Body); err != nil {
-		return
-	}
-
-	if addr := net.ParseIP(string(body)); addr != nil {
+	if addr := net.ParseIP(string(exIP)); addr != nil {
 		addrs = append(addrs, addr)
 	}
 
-	return addrs, err
+	return addrs, nil
 }
 
 // KernelArgs implements the runtime.Platform interface.
-func (a *Openstack) KernelArgs() procfs.Parameters {
+func (o *Openstack) KernelArgs() procfs.Parameters {
 	return []*procfs.Parameter{
 		procfs.NewParameter("console").Append("tty1").Append("ttyS0"),
 	}
