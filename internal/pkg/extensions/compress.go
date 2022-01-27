df0e388a4fa1995124d4e722fc1b8a1bfdffed58
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2022-01-27 19:15:25 +0300

    feat: extract firmware part of system extensions into initramfs
    Fixes #4816
    
    This changes the way system extensions are packaged into the squashfs
    images: `/lib/firmware` is now moved out of the future squashfs images
    and becomes part of `initramfs` to make firmware available in the early
    boot.
    
    Talos will bind-mount `/lib/firmware` into rootfs as well, so it will be
    available in the rootfs as well.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- internal/pkg/extensions/compress.go
+++ internal/pkg/extensions/compress.go
@@ -6,17 +6,91 @@ package extensions
 
 import (
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/talos-systems/talos/pkg/machinery/constants"
 )
 
-// Compress builds the squashfs image in the specified destination folder.
-func (ext *Extension) Compress(destinationPath string) (string, error) {
-	destinationPath = filepath.Join(destinationPath, fmt.Sprintf("%s.sqsh", ext.directory))
+// List of paths to be moved to the future initramfs.
+var initramfsPaths = []string{
+	constants.FirmwarePath,
+}
 
-	cmd := exec.Command("mksquashfs", ext.rootfsPath, destinationPath, "-all-root", "-noappend", "-comp", "xz", "-Xdict-size", "100%", "-no-progress")
+// Compress builds the squashfs image in the specified destination folder.
+//
+// Components which should be placed to the initramfs are moved to the initramfsPath.
+func (ext *Extension) Compress(squashPath, initramfsPath string) (string, error) {
+	for _, path := range initramfsPaths {
+		if _, err := os.Stat(filepath.Join(ext.rootfsPath, path)); err == nil {
+			if err = moveFiles(filepath.Join(ext.rootfsPath, path), filepath.Join(initramfsPath, path)); err != nil {
+				return "", err
+			}
+		}
+	}
+
+	squashPath = filepath.Join(squashPath, fmt.Sprintf("%s.sqsh", ext.directory))
+
+	cmd := exec.Command("mksquashfs", ext.rootfsPath, squashPath, "-all-root", "-noappend", "-comp", "xz", "-Xdict-size", "100%", "-no-progress")
 	cmd.Stderr = os.Stderr
 
-	return destinationPath, cmd.Run()
+	return squashPath, cmd.Run()
+}
+
+func moveFiles(srcPath, dstPath string) error {
+	st, err := os.Stat(srcPath)
+	if err != nil {
+		return err
+	}
+
+	if st.IsDir() {
+		return moveDirectory(st, srcPath, dstPath)
+	}
+
+	return moveFile(st, srcPath, dstPath)
+}
+
+func moveFile(st fs.FileInfo, srcPath, dstPath string) error {
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+
+	defer src.Close() //nolint:errcheck
+
+	dst, err := os.OpenFile(dstPath, os.O_CREATE|os.O_WRONLY, st.Mode().Perm())
+	if err != nil {
+		return err
+	}
+
+	defer dst.Close() //nolint:errcheck
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return os.Remove(srcPath)
+}
+
+func moveDirectory(st fs.FileInfo, srcPath, dstPath string) error {
+	if err := os.MkdirAll(dstPath, st.Mode().Perm()); err != nil {
+		return err
+	}
+
+	contents, err := os.ReadDir(srcPath)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range contents {
+		if err = moveFiles(filepath.Join(srcPath, item.Name()), filepath.Join(dstPath, item.Name())); err != nil {
+			return err
+		}
+	}
+
+	return os.Remove(srcPath)
 }
