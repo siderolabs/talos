93020583195d066e879ccb19da38b8cfd6b93e96
Noel Georgi <git@frezbo.dev>
2022-02-07 20:05:18 +0530

    chore: update kernel to 5.15.21
    Ump kernel to 5.15.21 stable
    
    Ref: https://github.com/talos-systems/pkgs/pull/390
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "5.15.19-talos"
+	DefaultKernelVersion = "5.15.21-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
