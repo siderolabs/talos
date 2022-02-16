a397024ae5587ff680f7c6c2cdef83631934a0e5
Serge Logvinov <serge.logvinov@sinextra.dev>
2022-02-16 18:15:59 +0000

    feat: add low source port capabilities
    This feature allow to us use low source port <1024 to make a http calls.
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>

--- pkg/download/download.go
+++ pkg/download/download.go
@@ -9,18 +9,23 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"net"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/talos-systems/go-retry/retry"
+	"golang.org/x/net/http/httpproxy"
 )
 
 const b64 = "base64"
 
 type downloadOptions struct {
-	Headers map[string]string
-	Format  string
+	Headers    map[string]string
+	Format     string
+	LowSrcPort bool
 
 	ErrorOnNotFound      error
 	ErrorOnEmptyResponse error
@@ -54,6 +59,14 @@ func WithFormat(format string) Option {
 func WithHeaders(headers map[string]string) Option {
 	return func(d *downloadOptions) {
 		d.Headers = headers
+	}
+}
+
+// WithLowSrcPort sets low source port to download
+// the config.
+func WithLowSrcPort() Option {
+	return func(d *downloadOptions) {
+		d.LowSrcPort = true
 	}
 }
 
@@ -129,6 +142,34 @@ func Download(ctx context.Context, endpoint string, opts ...Option) (b []byte, e
 
 func download(req *http.Request, dlOpts *downloadOptions) (data []byte, err error) {
 	client := &http.Client{}
+
+	if dlOpts.LowSrcPort {
+		port := 100 + rand.Intn(512)
+
+		localTCPAddr, tcperr := net.ResolveTCPAddr("tcp", ":"+strconv.Itoa(port))
+		if tcperr != nil {
+			return nil, retry.ExpectedError(fmt.Errorf("resolving source tcp address: %s", tcperr.Error()))
+		}
+
+		client.Transport = &http.Transport{
+			Proxy: func(req *http.Request) (*url.URL, error) {
+				return httpproxy.FromEnvironment().ProxyFunc()(req.URL)
+			},
+			DialContext: (&net.Dialer{
+				LocalAddr: localTCPAddr,
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			ForceAttemptHTTP2:     true,
+			DisableKeepAlives:     true,
+			MaxIdleConnsPerHost:   -1,
+		}
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
