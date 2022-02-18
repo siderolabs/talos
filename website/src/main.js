96e79a4417ee976eba4f7c16d43e606653fb2e10
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2022-02-18 22:18:43 +0500

    feat: update gridsome and add algolia search
    Signed-off-by: Andrew Rynhard <andrew@rynhard.io>

--- website/src/main.js
+++ website/src/main.js
@@ -5,38 +5,38 @@ import "~/assets/css/main.css";
 import Vuex from "vuex";
 import DefaultLayout from "~/layouts/Default.vue";
 
-export default function(Vue, { router, head, isClient, appOptions }) {
-  // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
+export default function (Vue, {router, head, isClient, appOptions}) {
+    // Set default layout as a global component
+    Vue.component("Layout", DefaultLayout);
 
-  Vue.use(Vuex);
+    Vue.use(Vuex);
 
-  appOptions.store = new Vuex.Store({
-    state: {
-      sidebarIsOpen: false,
-    },
+    appOptions.store = new Vuex.Store({
+        state: {
+            sidebarIsOpen: false,
+        },
 
-    mutations: {
-      setSidebarIsOpen(state, val) {
-        state.sidebarIsOpen = val;
-      },
-      toggleSidebarIsOpen(state) {
-        state.sidebarIsOpen = !state.sidebarIsOpen;
-      },
-    },
-  });
-
-  router.beforeEach((to, _from, next) => {
-    head.meta.push({
-      key: "og:url",
-      name: "og:url",
-      content: process.env.GRIDSOME_BASE_PATH + to.path,
+        mutations: {
+            setSidebarIsOpen(state, val) {
+                state.sidebarIsOpen = val;
+            },
+            toggleSidebarIsOpen(state) {
+                state.sidebarIsOpen = !state.sidebarIsOpen;
+            },
+        },
     });
-    next();
-  });
 
-  head.script.push({
-    src: "/js/asciinema-player.js",
-    body: true,
-  });
+    router.beforeEach((to, _from, next) => {
+        head.meta.push({
+            key: "og:url",
+            name: "og:url",
+            content: process.env.GRIDSOME_BASE_PATH + to.path,
+        });
+        next();
+    });
+
+    head.script.push({
+        src: "/js/asciinema-player.js",
+        body: true,
+    });
 }
