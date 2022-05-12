63f23e905e3dd3a9a42d82973fda16d110abbb1d
Serge Logvinov <serge.logvinov@sinextra.dev>
2022-05-12 05:02:26 +0000

    feat: openstack dhcp-v6 types
    Support ipv6_dhcp-* types of network.
    Apply static IPs in all supported network types.
    
    Signed-off-by: Serge Logvinov <serge.logvinov@sinextra.dev>
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

---
+++ internal/app/machined/pkg/runtime/v1alpha1/platform/utils/utils.go
@@ -0,0 +1,57 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package utils
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+
+	"inet.af/netaddr"
+)
+
+// IPPrefixFrom make netaddr.IPPrefix from cidr-address and netmask strings.
+// address can be IP or CIDR (1.1.1.1 or 1.1.1.1/8 or 1.1.1.1/255.0.0.0)
+// netmask can be IP or number (255.255.255.0 or 24 or empty).
+func IPPrefixFrom(address, netmask string) (netaddr.IPPrefix, error) {
+	cidr := strings.SplitN(address, "/", 2)
+	if len(cidr) == 1 {
+		address = cidr[0]
+	} else {
+		address = cidr[0]
+		netmask = cidr[1]
+	}
+
+	ip, err := netaddr.ParseIP(address)
+	if err != nil {
+		return netaddr.IPPrefix{}, fmt.Errorf("failed to parse ip address: %w", err)
+	}
+
+	if netmask == "" {
+		if ip.Is4() {
+			netmask = "32"
+		} else {
+			netmask = "128"
+		}
+	}
+
+	bits, err := strconv.Atoi(netmask)
+	if err != nil {
+		netmask, err := netaddr.ParseIP(netmask)
+		if err != nil {
+			return netaddr.IPPrefix{}, fmt.Errorf("failed to parse netmask: %w", err)
+		}
+
+		mask, _ := netmask.MarshalBinary() //nolint:errcheck // never fails
+		bits, _ = net.IPMask(mask).Size()
+	}
+
+	if ip.Is4() && bits > 32 {
+		return netaddr.IPPrefix{}, fmt.Errorf("failed netmask should be the same address family")
+	}
+
+	return netaddr.IPPrefixFrom(ip, uint8(bits)), nil
+}
