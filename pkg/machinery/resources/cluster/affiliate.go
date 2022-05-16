850cfba72fc89746367a845481f5dc1332a3e547
Dmitriy Matrenichev <dmitry.matrenichev@siderolabs.com>
2022-05-16 19:28:52 +0800

    chore: fix type order in deep-copy generation line
    Rewrite types in deep-copy generation line to ascending order. Coming from #5563.
    
    Signed-off-by: Dmitriy Matrenichev <dmitry.matrenichev@siderolabs.com>

--- pkg/machinery/resources/cluster/affiliate.go
+++ pkg/machinery/resources/cluster/affiliate.go
@@ -13,7 +13,7 @@ import (
 	"github.com/talos-systems/talos/pkg/machinery/config/types/v1alpha1/machine"
 )
 
-//go:generate deep-copy -type AffiliateSpec -type IdentitySpec -type MemberSpec -type ConfigSpec -header-file ../../../../hack/boilerplate.txt -o deep_copy.generated.go .
+//go:generate deep-copy -type AffiliateSpec -type ConfigSpec -type IdentitySpec -type MemberSpec -header-file ../../../../hack/boilerplate.txt -o deep_copy.generated.go .
 
 // AffiliateType is type of Affiliate resource.
 const AffiliateType = resource.Type("Affiliates.cluster.talos.dev")
