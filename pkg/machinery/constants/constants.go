e8113527f94f0fbc6cf6fdb9390dfb09d984213d
Noel Georgi <git@frezbo.dev>
2022-06-17 23:23:06 +0530

    chore: bump kubernetes to v1.24.2
    Bump kubernetes to [v1.24.2](https://github.com/kubernetes/kubernetes/releases/tag/v1.24.2)
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -257,7 +257,7 @@ const (
 	KubeletShutdownGracePeriodCriticalPods = 10 * time.Second
 
 	// DefaultKubernetesVersion is the default target version of the control plane.
-	DefaultKubernetesVersion = "1.24.1"
+	DefaultKubernetesVersion = "1.24.2"
 
 	// DefaultControlPlanePort is the default port to use for the control plane.
 	DefaultControlPlanePort = 6443
--- website/content/v1.2/_index.md
+++ website/content/v1.2/_index.md
@@ -6,7 +6,7 @@ cascade:
   type: docs
 preRelease: true
 lastRelease: v1.2.0-alpha.0
-kubernetesRelease: "1.24.1"
+kubernetesRelease: "1.24.2"
 prevKubernetesRelease: "1.24.1"
 ---
 
