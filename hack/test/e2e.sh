2c12fe59b368ad7c1753b53bf7b33d06964d95bc
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2022-10-17 14:31:00 +0400

    feat: update Kubernetes to 1.25.3
    See http://github.com/kubernetes/kubernetes/releases/tag/v1.25.3
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>

--- hack/test/e2e.sh
+++ hack/test/e2e.sh
@@ -30,7 +30,7 @@ export TALOS_VERSION=v1.1
 # Kubernetes
 
 export KUBECONFIG="${TMP}/kubeconfig"
-export KUBERNETES_VERSION=${KUBERNETES_VERSION:-1.25.2}
+export KUBERNETES_VERSION=${KUBERNETES_VERSION:-1.25.3}
 
 export NAME_PREFIX="talos-e2e-${SHA}-${PLATFORM}"
 export TIMEOUT=1200
