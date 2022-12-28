a2f7901166398fcfbfa69b971a7a9205b6835fec
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2022-12-28 19:05:04 +0400

    feat: include Kubernetes controlplane endpoint as one of the endpoints
    These endpoints are used for workers to find the addresses of the
    controlplane nodes to connect to `trustd` to issue certificates of
    `apid`.
    
    These endpoints today come from two sources:
    
    * discovery service data
    * Kubernetes API server endpoints
    
    This PR adds to the list static entry based on the Kubernetes control
    plane endpoint in the machine config.
    
    E.g. if the loadbalancer is used for the controlplane endpoint, and that
    loadbalancer also proxies requests for port 50001 (trustd), this static
    endpoint will provide workers with connectivity to trustd even if the
    discovery service is disabled, and Kubernetes API is not up.
    
    If this endpoint doesn't provide any trustd API, Talos will still try
    other endpoints.
    
    Talos does server certificate validation when calling trustd,
    so including malicious endpoints doesn't cause any harm, as malicious
    endpoint can't provider proper server certificate.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>
    (cherry picked from commit 80fed319408be9e493141fb2c01e5731708835c7)

---
+++ internal/app/machined/pkg/controllers/k8s/static_endpoint_test.go
@@ -0,0 +1,62 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package k8s_test
+
+import (
+	"net/netip"
+	"net/url"
+	"testing"
+
+	"github.com/cosi-project/runtime/pkg/resource"
+	"github.com/cosi-project/runtime/pkg/resource/rtestutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/siderolabs/talos/internal/app/machined/pkg/controllers/ctest"
+	k8sctrl "github.com/siderolabs/talos/internal/app/machined/pkg/controllers/k8s"
+	"github.com/siderolabs/talos/pkg/machinery/config/types/v1alpha1"
+	"github.com/siderolabs/talos/pkg/machinery/resources/config"
+	"github.com/siderolabs/talos/pkg/machinery/resources/k8s"
+)
+
+type StaticEndpointControllerSuite struct {
+	ctest.DefaultSuite
+}
+
+func (suite *StaticEndpointControllerSuite) TestReconcile() {
+	u, err := url.Parse("https://[2001:db8::1]:6443/")
+	suite.Require().NoError(err)
+
+	cfg := config.NewMachineConfig(
+		&v1alpha1.Config{
+			ConfigVersion: "v1alpha1",
+			MachineConfig: &v1alpha1.MachineConfig{},
+			ClusterConfig: &v1alpha1.ClusterConfig{
+				ControlPlane: &v1alpha1.ControlPlaneConfig{
+					Endpoint: &v1alpha1.Endpoint{
+						URL: u,
+					},
+				},
+			},
+		},
+	)
+
+	suite.Require().NoError(suite.State().Create(suite.Ctx(), cfg))
+
+	rtestutils.AssertResources(suite.Ctx(), suite.T(), suite.State(), []resource.ID{k8s.ControlPlaneKubernetesEndpointsID},
+		func(endpoint *k8s.Endpoint, assert *assert.Assertions) {
+			assert.Equal([]netip.Addr{netip.MustParseAddr("2001:db8::1")}, endpoint.TypedSpec().Addresses)
+		})
+}
+
+func TestStaticEndpointControllerSuite(t *testing.T) {
+	suite.Run(t, &StaticEndpointControllerSuite{
+		DefaultSuite: ctest.DefaultSuite{
+			AfterSetup: func(suite *ctest.DefaultSuite) {
+				suite.Require().NoError(suite.Runtime().RegisterController(&k8sctrl.StaticEndpointController{}))
+			},
+		},
+	})
+}
