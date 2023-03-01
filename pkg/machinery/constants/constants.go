042adbf8a3bf8bc71b106c89f4176685b0c81e50
Andrey Smirnov <andrey.smirnov@talos-systems.com>
2023-03-01 22:50:54 +0400

    feat: update Kubernetes to v1.26.2
    See https://github.com/kubernetes/kubernetes/releases/tag/v1.26.2
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@talos-systems.com>
    (cherry picked from commit dea17d72340b1deddea2215b556a4f193d1feb7f)

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -294,7 +294,7 @@ const (
 	SeccompProfilesDirectory = "/var/lib/kubelet/seccomp/profiles"
 
 	// DefaultKubernetesVersion is the default target version of the control plane.
-	DefaultKubernetesVersion = "1.26.1"
+	DefaultKubernetesVersion = "1.26.2"
 
 	// DefaultControlPlanePort is the default port to use for the control plane.
 	DefaultControlPlanePort = 6443
