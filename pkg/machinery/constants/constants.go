5934815d2fe975c4d8ddb2a26ef733d29565cdb2
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2023-12-13 20:55:17 +0400

    chore: split more kernel modules on amd64
    See https://github.com/siderolabs/pkgs/pull/844
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "6.1.65-talos"
+	DefaultKernelVersion = "6.1.67-talos"
 
 	// KernelModulesPath is the default path to the kernel modules without the kernel version.
 	KernelModulesPath = "/lib/modules"
