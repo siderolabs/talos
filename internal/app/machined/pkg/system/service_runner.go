siderolabs/talos

commit 07b91797caf81162b0e491f749bf00003d3d33a4

fix: report internally service as unhealthy if not running
Otherwise the internal code might assume that the service is still
running and healthy, never issuing a health change event.

Fixes #9271

Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

internal/app/machined/pkg/system/service_runner.go
@@ -282,6 +282,10 @@ func (svcrunner *ServiceRunner) run(ctx context.Context, runnr runner.Runner) er
 	go func() {
 		errCh <- runnr.Run(func(s events.ServiceState, msg string, args ...any) {
 			svcrunner.UpdateState(ctx, s, msg, args...)
+
+			if s != events.StateRunning {
+				svcrunner.healthState.Update(false, "service not running")
+			}
 		})
 	}()
 

commit 95c69588060239357bb0c1f43732d560b388429e

fix: don't reset health status if service doesn't support health checks
Fixes #9839

Bug was introduced in #9273

Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

internal/app/machined/pkg/system/service_runner.go
@@ -283,7 +283,7 @@ func (svcrunner *ServiceRunner) run(ctx context.Context, runnr runner.Runner) er
 		errCh <- runnr.Run(func(s events.ServiceState, msg string, args ...any) {
 			svcrunner.UpdateState(ctx, s, msg, args...)
 
-			if s != events.StateRunning {
+			if _, healthSupported := svcrunner.service.(HealthcheckedService); healthSupported && s != events.StateRunning {
 				svcrunner.healthState.Update(false, "service not running")
 			}
 		})