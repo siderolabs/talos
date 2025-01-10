0c05e1cd3cf13c292c90e1ab55ff96a1807cc48c
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2025-01-10 20:18:43 +0400

    feat: update Linux to 6.12.8
    Also update runc to 1.2.4.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>
    (cherry picked from commit 1be5f8ff25ac7042ee3334f657d6604ec5f8501d)

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ TOOLS ?= ghcr.io/siderolabs/tools:v1.9.0-1-geaad82f
 DEBUG_TOOLS_SOURCE := scratch
 
 PKGS_PREFIX ?= ghcr.io/siderolabs
-PKGS ?= v1.9.0-15-g45c4ba4
+PKGS ?= v1.9.0-21-gc1f06e5
 EXTRAS ?= v1.9.0
 
 KRES_IMAGE ?= ghcr.io/siderolabs/kres:latest
--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "6.12.6-talos"
+	DefaultKernelVersion = "6.12.8-talos"
 
 	// KernelModulesPath is the default path to the kernel modules without the kernel version.
 	KernelModulesPath = "/lib/modules"
