9b9512ba88e0f2ebd78b72f4abdccb3f46f9320b
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2025-03-18 16:13:23 +0400

    feat: update Linux 6.12.19, containerd 2.0.4
    Also update runc to 1.2.6.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "6.12.18-talos"
+	DefaultKernelVersion = "6.12.19-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
@@ -540,7 +540,7 @@ const (
 	TrustdUserID = 51
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "2.0.3"
+	DefaultContainerdVersion = "2.0.4"
 
 	// SystemContainerdNamespace is the Containerd namespace for Talos services.
 	SystemContainerdNamespace = "system"
