siderolabs/talos

commit 7e7804b7af9c74f2030337f79dbe0a2142cdf40c

fix: avoid printing terminating null byte in SELinux context
SELinux context (file extended attribute `security.selinux`) is a
null-terminated string. We need to properly convert the C-string inside
the byte slice into a Golang string to avoid also printing the null byte
in the ls command output.

Discovered when piping talosctl ls output to grep:

    $ talosctl ls /lib/modules/ -r -l | grep '\.ko'
    grep: (standard input): binary file matches

Signed-off-by: Joakim Nohlgård <joakim@nohlgard.se>

cmd/talosctl/cmd/talos/list.go
@@ -16,6 +16,7 @@ import (
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
 
 	"github.com/siderolabs/talos/cmd/talosctl/pkg/talos/helpers"
 	machineapi "github.com/siderolabs/talos/pkg/machinery/api/machine"
@@ -153,7 +154,7 @@ var lsCmd = &cobra.Command{
 				if info.Xattrs != nil {
 					for _, l := range info.Xattrs {
 						if l.Name == "security.selinux" {
-							label = string(l.Data)
+							label = unix.ByteSliceToString(l.Data)
 
 							break
 						}

commit c4136c27dab4152114e386b07c6d004acbe9af25

fix: uki boot detection
Fix UKI boot detection

Also fix bug introduced by #10640 which imported the unix package making
talosctl non-unix builds broken.

Signed-off-by: Noel Georgi <git@frezbo.dev>

.github/workflows/ci.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-04-04T12:28:10Z by kres d903dae.
+# Generated on 2025-04-08T03:31:36Z by kres d903dae.
 
 name: default
 concurrency:
@@ -1448,8 +1448,8 @@ jobs:
         uses: actions/checkout@v4
         with:
           path: _out/contrib
-          ref: main
-          repository: siderolabs/contrib
+          ref: feat/enable-gcp-uefi
+          repository: frezbo/contrib
       - name: setup tf
         uses: hashicorp/setup-terraform@v3
         with:

.github/workflows/integration-gcp-cron.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-04-01T08:14:24Z by kres d903dae.
+# Generated on 2025-04-08T03:31:36Z by kres d903dae.
 
 name: integration-gcp-cron
 concurrency:
@@ -104,8 +104,8 @@ jobs:
         uses: actions/checkout@v4
         with:
           path: _out/contrib
-          ref: main
-          repository: siderolabs/contrib
+          ref: feat/enable-gcp-uefi
+          repository: frezbo/contrib
       - name: setup tf
         uses: hashicorp/setup-terraform@v3
         with:

.kres.yaml
@@ -2720,8 +2720,8 @@ spec:
         - name: e2e-gcp-prepare
         - name: checkout contrib
           checkoutStep:
-            repository: siderolabs/contrib
-            ref: main
+            repository: frezbo/contrib
+            ref: feat/enable-gcp-uefi
             path: _out/contrib
         - name: setup tf
           terraformStep: true

cmd/talosctl/cmd/talos/list.go
@@ -5,6 +5,7 @@
 package talos
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -16,7 +17,6 @@ import (
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/spf13/cobra"
-	"golang.org/x/sys/unix"
 
 	"github.com/siderolabs/talos/cmd/talosctl/pkg/talos/helpers"
 	machineapi "github.com/siderolabs/talos/pkg/machinery/api/machine"
@@ -154,7 +154,7 @@ var lsCmd = &cobra.Command{
 				if info.Xattrs != nil {
 					for _, l := range info.Xattrs {
 						if l.Name == "security.selinux" {
-							label = unix.ByteSliceToString(l.Data)
+							label = string(bytes.Trim(l.Data, "\x00\n"))
 
 							break
 						}

hack/cloud-image-uploader/gcp.go
@@ -253,11 +253,15 @@ func (u *GCPUploder) insertImage(imageName, arch string) (operationID, imageLink
 			{
 				Type: "VIRTIO_SCSI_MULTIQUEUE",
 			},
+			{
+				Type: "UEFI_COMPATIBLE",
+			},
 		},
 		Name: imageName,
 		RawDisk: &compute.ImageRawDisk{
 			Source: u.imagePath,
 		},
+		ShieldedInstanceInitialState: &compute.InitialStateConfig{},
 	}).Do()
 	if err != nil {
 		return "", "", fmt.Errorf("gcp: failed to insert image: %w", err)

internal/app/machined/pkg/controllers/runtime/security_state.go
@@ -14,6 +14,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/cosi-project/runtime/pkg/controller"
@@ -95,10 +96,23 @@ func (ctrl *SecurityStateController) Run(ctx context.Context, r controller.Runti
 				secureBootState = true
 			}
 
-			defaultEntry, err := sdboot.ReadVariable(efivario.NewDefaultContext(), sdboot.LoaderEntryDefaultName)
+			efiVarCtx := efivario.NewDefaultContext()
+
+			defaultEntry, err := sdboot.ReadVariable(efiVarCtx, sdboot.LoaderEntryDefaultName)
 			if err == nil {
 				if strings.HasPrefix(defaultEntry, "Talos-") {
 					bootedWithUKI = true
+				}
+			}
+
+			// if defaultEntry is empty in the case when we booted off a disk image when installer never runs, we can rely on the
+			// stub image identifier to determine if we booted with UKI
+			if defaultEntry == "" {
+				stubImageIdentifier, err := sdboot.ReadVariable(efiVarCtx, sdboot.StubImageIdentifierName)
+				if err == nil {
+					if strings.HasPrefix(filepath.Base(strings.ReplaceAll(stubImageIdentifier, "\\", "/")), "Talos-") {
+						bootedWithUKI = true
+					}
 				}
 			}
 

internal/app/machined/pkg/runtime/v1alpha1/bootloader/sdboot/efivars.go
@@ -30,6 +30,8 @@ const (
 	LoaderEntryDefaultName  = "LoaderEntryDefault"
 	LoaderEntrySelectedName = "LoaderEntrySelected"
 	LoaderConfigTimeoutName = "LoaderConfigTimeout"
+
+	StubImageIdentifierName = "StubImageIdentifier"
 )
 
 // ReadVariable reads a SystemdBoot EFI variable.

internal/integration/api/common.go
@@ -62,7 +62,7 @@ func (suite *CommonSuite) TestVirtioModulesLoaded() {
 
 // TestCommonDefaults verifies that the default ulimits are set.
 func (suite *CommonSuite) TestCommonDefaults() {
-	if suite.Cluster != nil && suite.Cluster.Provisioner() == "docker" {
+	if suite.Cluster != nil && suite.Cluster.Provisioner() == base.ProvisionerDocker {
 		suite.T().Skip("skipping ulimits test since provisioner is docker")
 	}
 
@@ -162,7 +162,7 @@ func (suite *CommonSuite) TestDNSResolver() {
 
 // TestBaseOCISpec verifies that the base OCI spec can be modified.
 func (suite *CommonSuite) TestBaseOCISpec() {
-	if suite.Cluster != nil && suite.Cluster.Provisioner() == "docker" {
+	if suite.Cluster != nil && suite.Cluster.Provisioner() == base.ProvisionerDocker {
 		suite.T().Skip("skipping ulimits test since provisioner is docker")
 	}
 

internal/integration/api/uki.go
@@ -34,8 +34,8 @@ func (suite *UKISuite) SuiteName() string {
 
 // SetupTest sets up the test.
 func (suite *UKISuite) SetupTest() {
-	if suite.Cluster.Provisioner() != base.ProvisionerQEMU {
-		suite.T().Skip("skipping uki booted test since not using qemu provisioner")
+	if suite.Cluster != nil && suite.Cluster.Provisioner() == base.ProvisionerDocker {
+		suite.T().Skip("skipping uki booted test since docker provisioner does not support UKI")
 	}
 
 	if !suite.VerifyUKIBooted {

internal/integration/base/base.go
@@ -18,6 +18,8 @@ import (
 const (
 	// ProvisionerQEMU is the name of the QEMU provisioner.
 	ProvisionerQEMU = "qemu"
+	// ProvisionerDocker is the name of the Docker provisioner.
+	ProvisionerDocker = "docker"
 )
 
 // TalosSuite defines most common settings for integration test suites.

internal/integration/integration_test.go
@@ -88,7 +88,7 @@ func TestIntegration(t *testing.T) {
 			t.Error("error reflecting cluster via provisioner", err)
 		}
 
-		if k8sEndpoint == "" && provisionerName == "docker" {
+		if k8sEndpoint == "" && provisionerName == base.ProvisionerDocker {
 			k8sEndpoint = cluster.Info().KubernetesEndpoint
 		}
 	}