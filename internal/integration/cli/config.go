e37573ec2ec3424e68c731d3f568bfb35f79477d
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2025-04-30 13:17:59 +0400

    fix: relax etcd APIs RBAC requirements
    Allow more read-only APIs to be used with `os:reader`/`os:etcd:backup`.
    
    Fixes #10821
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>
    (cherry picked from commit 2b89c2810551ab52678e62fcbf5355dd05c72030)

--- internal/integration/cli/config.go
+++ internal/integration/cli/config.go
@@ -249,7 +249,7 @@ func (suite *TalosconfigSuite) TestNew() {
 		nonprivOpts []base.RunOption
 	}{
 		{
-			args: []string{"etcd", "alarm", "list"},
+			args: []string{"etcd", "alarm", "disarm"},
 			privOpts: []base.RunOption{
 				base.StdoutEmpty(),
 			},
