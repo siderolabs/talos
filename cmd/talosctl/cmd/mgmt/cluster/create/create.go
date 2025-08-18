siderolabs/talos

commit ea8289f550787593b1cd35f2d8da59aa5311880e

feat: add a user facing docker command
Add a user facing cluster create docker command with the following changes:
* renamed flags for simplicity and uniformity
* removed the bulk of the unnecessary flags

Other changes:
* split internal logic such that it's separate from the qemu cluster create logic
* refactor internal code aiming for simplicity
Ä change drives flag behavior in anticipation of the user facing create-qemu command
* extract code into separate functions
* add some unit tests
* remove the docker support from the cluster create command (docker is only supported via the user-facing create-docker command)

Signed-off-by: Orzelius <33936483+Orzelius@users.noreply.github.com>

cmd/talosctl/cmd/mgmt/cluster/create/create_docker.go
@@ -1,0 +1,158 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package create
+
+import (
+	"fmt"
+	"net/netip"
+	"strings"
+
+	sideronet "github.com/siderolabs/net"
+
+	"github.com/siderolabs/talos/cmd/talosctl/pkg/mgmt/helpers"
+	clientconfig "github.com/siderolabs/talos/pkg/machinery/client/config"
+	"github.com/siderolabs/talos/pkg/machinery/config"
+	"github.com/siderolabs/talos/pkg/machinery/config/bundle"
+	"github.com/siderolabs/talos/pkg/machinery/config/generate"
+	"github.com/siderolabs/talos/pkg/provision"
+)
+
+type clusterCreateRequestData struct {
+	clusterRequest   provision.ClusterRequest
+	provisionOptions []provision.Option
+	talosconfig      *clientconfig.Config
+}
+
+//nolint:gocyclo,cyclop
+func getDockerClusterRequest(
+	cOps commonOps,
+	dOps dockerOps,
+	provisioner provision.Provisioner,
+) (clusterCreateRequestData, error) {
+	parts := strings.Split(dOps.talosImage, ":")
+	cOps.talosVersion = parts[len(parts)-1]
+
+	_, err := config.ParseContractFromVersion(cOps.talosVersion)
+	if err != nil {
+		currentVersion := helpers.GetTag()
+		cOps.talosVersion = currentVersion
+		fmt.Printf("failed to derrive Talos version from the docker image, defaulting to %s\n", currentVersion)
+	}
+
+	controlplaneResources, err := parseResources(cOps.controlplaneResources)
+	if err != nil {
+		return clusterCreateRequestData{}, fmt.Errorf("error parsing controlplane resources: %s", err)
+	}
+
+	workerResources, err := parseResources(cOps.workerResources)
+	if err != nil {
+		return clusterCreateRequestData{}, fmt.Errorf("error parsing worker resources: %s", err)
+	}
+
+	cidr, err := getCidr4(cOps)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	nodeIPs, err := getIps(cidr, cOps)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	gatewayIP, err := sideronet.NthIPInNetwork(cidr, gatewayOffset)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	clusterRequest := getBaseClusterRequest(cOps, []netip.Prefix{cidr}, []netip.Addr{gatewayIP})
+
+	genOptions := []generate.Option{}
+
+	registryMirrorOps, err := getRegistryMirrorGenOps(cOps)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	genOptions = append(genOptions, registryMirrorOps...)
+	genOptions = append(genOptions, provisioner.GenOptions(clusterRequest.Network)...)
+
+	versionContractGenOps, _, err := getVersionContractGenOps(cOps)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	genOptions = append(genOptions, versionContractGenOps)
+	genOptions = append(genOptions, provisioner.GenOptions(clusterRequest.Network)...)
+
+	endpointList := provisioner.GetTalosAPIEndpoints(clusterRequest.Network)
+	genOptions = append(genOptions, getWithAdditionalSubjectAltNamesGenOps(endpointList)...)
+	genOptions = append(genOptions, generate.WithEndpointList(endpointList))
+
+	configBundleOpts := []bundle.Option{}
+	configBundleOpts = append(configBundleOpts,
+		bundle.WithInputOptions(
+			&bundle.InputOptions{
+				ClusterName: cOps.rootOps.ClusterName,
+				Endpoint:    provisioner.GetInClusterKubernetesControlPlaneEndpoint(clusterRequest.Network, cOps.controlPlanePort),
+				KubeVersion: strings.TrimPrefix(cOps.kubernetesVersion, "v"),
+				GenOptions:  genOptions,
+			}),
+	)
+
+	provisionOptions := []provision.Option{
+		provision.WithDockerPortsHostIP(dOps.hostIP),
+		provision.WithKubernetesEndpoint(provisioner.GetExternalKubernetesControlPlaneEndpoint(clusterRequest.Network, cOps.controlPlanePort)),
+	}
+
+	if dOps.ports != "" {
+		portList := strings.Split(dOps.ports, ",")
+		provisionOptions = append(provisionOptions, provision.WithDockerPorts(portList))
+	}
+
+	configPatchBundleOps, err := getConfigPatchBundleOps(cOps)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	configBundleOpts = append(configBundleOpts, configPatchBundleOps...)
+
+	configBundle, err := bundle.NewBundle(configBundleOpts...)
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	bundleTalosconfig := configBundle.TalosConfig()
+
+	provisionOptions = append(provisionOptions, provision.WithTalosConfig(configBundle.TalosConfig()))
+
+	controlplanes, workers, err := createNodeRequests(cOps, controlplaneResources, workerResources, [][]netip.Addr{nodeIPs})
+	if err != nil {
+		return clusterCreateRequestData{}, err
+	}
+
+	for i := range controlplanes {
+		controlplanes[i].Config = configBundle.ControlPlane()
+	}
+
+	for i := range workers {
+		workers[i].Config = configBundle.Worker()
+	}
+
+	clusterRequest.Nodes = append(clusterRequest.Nodes, controlplanes...)
+	clusterRequest.Nodes = append(clusterRequest.Nodes, workers...)
+
+	for i := range clusterRequest.Nodes {
+		clusterRequest.Nodes[i].Mounts = dOps.mountOpts.Value()
+	}
+
+	clusterRequest.Network.DockerDisableIPv6 = dOps.disableIPv6
+	clusterRequest.Image = dOps.talosImage
+
+	return clusterCreateRequestData{
+		clusterRequest:   clusterRequest,
+		provisionOptions: provisionOptions,
+		talosconfig:      bundleTalosconfig,
+	}, nil
+}

cmd/talosctl/cmd/mgmt/cluster/cluster.go
@@ -49,7 +49,7 @@ func init() {
 		DefaultCNIDir = filepath.Join(talosDir, "cni")
 	}
 
-	Cmd.PersistentFlags().StringVar(&Flags.ProvisionerName, "provisioner", providers.DockerProviderName, "Talos cluster provisioner to use")
+	Cmd.PersistentFlags().StringVar(&Flags.ProvisionerName, ProvisionerFlag, providers.DockerProviderName, "Talos cluster provisioner to use")
 	Cmd.PersistentFlags().StringVar(&Flags.StateDir, "state", DefaultStateDir, "directory path to store cluster state")
 	Cmd.PersistentFlags().StringVar(&Flags.ClusterName, "name", "talos-default", "the name of the cluster")
 }

cmd/talosctl/cmd/mgmt/cluster/create/cmd.go
@@ -1,0 +1,125 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package create
+
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/spf13/pflag"
+
+	clustercmd "github.com/siderolabs/talos/cmd/talosctl/cmd/mgmt/cluster"
+	"github.com/siderolabs/talos/pkg/machinery/constants"
+)
+
+var (
+	workersFlagName           = "workers"
+	controlplanesFlagName     = "controlplanes"
+	kubernetesVersionFlagName = "kubernetes-version"
+	registryMirrorFlagName    = "registry-mirror"
+	networkMTUFlagName        = "mtu"
+
+	// user facing command flags.
+	talosconfigDestinationFlagName = "talosconfig-destination"
+)
+
+// commonOps are the options that are not specific to a single provider.
+type commonOps struct {
+	// RootOps are the options from the root cluster command
+	rootOps                   *clustercmd.CmdOps
+	talosconfigDestination    string
+	registryMirrors           []string
+	registryInsecure          []string
+	kubernetesVersion         string
+	applyConfigEnabled        bool
+	configDebug               bool
+	networkCIDR               string
+	networkMTU                int
+	networkIPv4               bool
+	dnsDomain                 string
+	workers                   int
+	controlplanes             int
+	controlplaneResources     nodeResources
+	workerResources           nodeResources
+	clusterWait               bool
+	clusterWaitTimeout        time.Duration
+	forceInitNodeAsEndpoint   bool
+	forceEndpoint             string
+	inputDir                  string
+	controlPlanePort          int
+	withInitNode              bool
+	customCNIUrl              string
+	skipKubeconfig            bool
+	skipInjectingConfig       bool
+	talosVersion              string
+	enableKubeSpan            bool
+	enableClusterDiscovery    bool
+	configPatch               []string
+	configPatchControlPlane   []string
+	configPatchWorker         []string
+	kubePrismPort             int
+	skipK8sNodeReadinessCheck bool
+	withJSONLogs              bool
+	wireguardCIDR             string
+}
+
+func addTalosconfigDestinationFlag(flagset *pflag.FlagSet, bind *string, flagName string) {
+	flagset.StringVar(bind, flagName, "",
+		fmt.Sprintf("The location to save the generated Talos configuration file to. Defaults to '%s' env variable if set, otherwise '%s' and '%s' in order.",
+			constants.TalosConfigEnvVar,
+			filepath.Join("$HOME", constants.TalosDir, constants.TalosconfigFilename),
+			filepath.Join(constants.ServiceAccountMountPath, constants.TalosconfigFilename),
+		),
+	)
+}
+
+func addControlplaneCpusFlag(flagset *pflag.FlagSet, bind *string, flagName string) {
+	flagset.StringVar(bind, flagName, "2.0", "the share of CPUs as fraction (each control plane/VM)")
+}
+
+func addWorkersCpusFlag(flagset *pflag.FlagSet, bind *string, flagName string) {
+	flagset.StringVar(bind, flagName, "2.0", "the share of CPUs as fraction (each worker/VM)")
+}
+
+func addControlPlaneMemoryFlag(flagset *pflag.FlagSet, bind *int, flagName string) {
+	flagset.IntVar(bind, flagName, 2048, "the limit on memory usage in MB (each control plane/VM)")
+}
+
+func addWorkersMemoryFlag(flagset *pflag.FlagSet, bind *int, flagName string) {
+	flagset.IntVar(bind, flagName, 2048, "the limit on memory usage in MB (each worker/VM)")
+}
+
+func addConfigPatchFlag(flagset *pflag.FlagSet, bind *[]string, flagName string) {
+	flagset.StringArrayVar(bind, flagName, nil, "patch generated machineconfigs (applied to all node types), use @file to read a patch from file")
+}
+
+func addConfigPatchControlPlaneFlag(flagset *pflag.FlagSet, bind *[]string, flagName string) {
+	flagset.StringArrayVar(bind, flagName, nil, "patch generated machineconfigs (applied to 'init' and 'controlplane' types)")
+}
+
+func addConfigPatchWorkerFlag(flagset *pflag.FlagSet, bind *[]string, flagName string) {
+	flagset.StringArrayVar(bind, flagName, nil, "patch generated machineconfigs (applied to 'worker' type)")
+}
+
+func addWorkersFlag(flagset *pflag.FlagSet, bind *int) {
+	flagset.IntVar(bind, workersFlagName, 1, "the number of workers to create")
+}
+
+func addControlplanesFlag(flagset *pflag.FlagSet, bind *int) {
+	flagset.IntVar(bind, controlplanesFlagName, 1, "the number of controlplanes to create")
+}
+
+func addKubernetesVersionFlag(flagset *pflag.FlagSet, bind *string) {
+	flagset.StringVar(bind, kubernetesVersionFlagName, constants.DefaultKubernetesVersion, "desired kubernetes version to run")
+}
+
+func addRegistryMirrorFlag(flagset *pflag.FlagSet, bind *[]string) {
+	flagset.StringSliceVar(bind, registryMirrorFlagName, []string{}, "list of registry mirrors to use in format: <registry host>=<mirror URL>")
+}
+
+func addNetworkMTUFlag(flagset *pflag.FlagSet, bind *int) {
+	flagset.IntVar(bind, networkMTUFlagName, 1500, "MTU of the cluster network")
+}

cmd/talosctl/cmd/mgmt/cluster/create/cmd_docker.go
@@ -1,0 +1,139 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package create
+
+import (
+	"context"
+	"time"
+
+	"github.com/docker/cli/opts"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+
+	clustercmd "github.com/siderolabs/talos/cmd/talosctl/cmd/mgmt/cluster"
+	"github.com/siderolabs/talos/cmd/talosctl/pkg/mgmt/helpers"
+	"github.com/siderolabs/talos/pkg/cli"
+	"github.com/siderolabs/talos/pkg/images"
+	"github.com/siderolabs/talos/pkg/machinery/constants"
+	"github.com/siderolabs/talos/pkg/provision/providers"
+)
+
+type dockerOps struct {
+	hostIP      string
+	disableIPv6 bool
+	mountOpts   opts.MountOpt
+	ports       string
+	talosImage  string
+}
+
+func init() {
+	ops := &createOps{
+		common: commonOps{
+			controlplanes:      1,
+			networkMTU:         1500,
+			clusterWaitTimeout: 20 * time.Minute,
+			clusterWait:        true,
+			dnsDomain:          "cluster.local",
+			controlPlanePort:   constants.DefaultControlPlanePort,
+			rootOps:            &clustercmd.Flags,
+			networkIPv4:        true,
+		},
+		docker: dockerOps{},
+	}
+
+	const (
+		controlPlaneCpusFlag   = "cpus-controlplanes"
+		controlPlaneMemoryFlag = "memory-controlplanes"
+		workersCpusFlag        = "cpus-workers"
+		workersMemoryFlag      = "memory-workers"
+
+		configPatchFlag             = "config-patch"
+		configPatchControlPlaneFlag = "config-patch-controlplanes"
+		configPatchWorkerFlag       = "config-patch-workers"
+
+		// docker specific flags
+		portsFlag             = "exposed-ports"
+		dockerDisableIPv6Flag = "disable-ipv6"
+		dockerHostIPFlag      = "host-ip"
+		mountOptsFlag         = "mount"
+		subnetFlag            = "subnet"
+	)
+
+	getDockerFlags := func() *pflag.FlagSet {
+		docker := pflag.NewFlagSet("docker", pflag.PanicOnError)
+
+		docker.StringVarP(&ops.docker.ports, portsFlag, "p", "",
+			"comma-separated list of ports/protocols to expose on init node. Ex -p <hostPort>:<containerPort>/<protocol (tcp or udp)>")
+		docker.StringVar(&ops.docker.hostIP, dockerHostIPFlag, "0.0.0.0", "Host IP to forward exposed ports to")
+		docker.BoolVar(&ops.docker.disableIPv6, dockerDisableIPv6Flag, false, "skip enabling IPv6 in containers")
+		cli.Should(docker.MarkHidden(dockerDisableIPv6Flag))
+		docker.Var(&ops.docker.mountOpts, mountOptsFlag, "attach a mount to the container (docker --mount syntax)")
+		docker.StringVar(&ops.docker.talosImage, "image", helpers.DefaultImage(images.DefaultTalosImageRepository), "the talos image to run")
+
+		return docker
+	}
+
+	getCommonFlags := func() *pflag.FlagSet {
+		common := pflag.NewFlagSet("common", pflag.PanicOnError)
+
+		common.StringVar(&ops.common.networkCIDR, subnetFlag, "10.5.0.0/24", "Docker network subnet CIDR")
+
+		addWorkersFlag(common, &ops.common.workers)
+		addKubernetesVersionFlag(common, &ops.common.kubernetesVersion)
+		addTalosconfigDestinationFlag(common, &ops.common.talosconfigDestination, talosconfigDestinationFlagName)
+		addConfigPatchFlag(common, &ops.common.configPatch, configPatchFlag)
+		addConfigPatchControlPlaneFlag(common, &ops.common.configPatchControlPlane, configPatchControlPlaneFlag)
+		addConfigPatchWorkerFlag(common, &ops.common.configPatchWorker, configPatchWorkerFlag)
+
+		// the following flags are used in tests
+		addNetworkMTUFlag(common, &ops.common.networkMTU)
+		cli.Should(common.MarkHidden(networkMTUFlagName))
+		addRegistryMirrorFlag(common, &ops.common.registryMirrors)
+		cli.Should(common.MarkHidden(registryMirrorFlagName))
+
+		addControlplaneCpusFlag(common, &ops.common.controlplaneResources.cpu, controlPlaneCpusFlag)
+		addWorkersCpusFlag(common, &ops.common.workerResources.cpu, workersCpusFlag)
+		addControlPlaneMemoryFlag(common, &ops.common.controlplaneResources.memory, controlPlaneMemoryFlag)
+		addWorkersMemoryFlag(common, &ops.common.workerResources.memory, workersMemoryFlag)
+
+		return common
+	}
+
+	createDockerCmd := &cobra.Command{
+		Use:   "docker",
+		Short: "Create a local Docker based kubernetes cluster",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cli.WithContext(context.Background(), func(ctx context.Context) error {
+				provisioner, err := providers.Factory(ctx, providers.DockerProviderName)
+				if err != nil {
+					return err
+				}
+
+				data, err := getDockerClusterRequest(ops.common, ops.docker, provisioner)
+				if err != nil {
+					return err
+				}
+
+				cluster, err := provisioner.Create(ctx, data.clusterRequest, data.provisionOptions...)
+				if err != nil {
+					return err
+				}
+				// Create and save the talosctl configuration file.
+				err = postCreate(ctx, ops.common, data.talosconfig, cluster, data.provisionOptions, data.clusterRequest)
+				if err != nil {
+					return err
+				}
+
+				return clustercmd.ShowCluster(cluster)
+			})
+		},
+	}
+
+	createDockerCmd.Flags().AddFlagSet(getDockerFlags())
+	createDockerCmd.Flags().AddFlagSet(getCommonFlags())
+
+	createCmd.AddCommand(createDockerCmd)
+}

commit 9c97ed886b89b2fb84f47866abdf1000839143c4

fix: version contract parsing in encryption keys handling
Fix issue introduced in #11532 (`main` only) with versionContract
parsing: wrong variable was returned (overwritten).

Also some small cleanups/nits (with Albert).

Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

cmd/talosctl/cmd/mgmt/cluster/create/create.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-getter/v2"
+	"github.com/siderolabs/gen/xslices"
 	sideronet "github.com/siderolabs/net"
 
 	"github.com/siderolabs/talos/pkg/cluster/check"
@@ -103,12 +104,9 @@ func createNodeRequests(cOps commonOps, controlplaneRes, workerRes parsedNodeRes
 }
 
 func getNodeIPs(ips [][]netip.Addr, nodeIndex int) []netip.Addr {
-	nodeIPs := make([]netip.Addr, len(ips))
-	for j := range nodeIPs {
-		nodeIPs[j] = ips[j][nodeIndex]
-	}
-
-	return nodeIPs
+	return xslices.Map(ips, func(ips []netip.Addr) netip.Addr {
+		return ips[nodeIndex]
+	})
 }
 
 // downloadBootAssets downloads the boot assets in the given qemuOps if they are URLs, and replaces their URL paths with the downloaded paths on the filesystem.
@@ -256,7 +254,7 @@ func parseCPUShare(cpus string) (int64, error) {
 }
 
 func getRegistryMirrorGenOps(cOps commonOps) ([]generate.Option, error) {
-	ops := []generate.Option{}
+	ops := make([]generate.Option, 0, len(cOps.registryMirrors))
 
 	for _, registryMirror := range cOps.registryMirrors {
 		left, right, ok := strings.Cut(registryMirror, "=")
@@ -286,36 +284,28 @@ func getBaseClusterRequest(cOps commonOps, cidrs []netip.Prefix, gatewayIPs []ne
 	}
 }
 
-func getVersionContractGenOps(cOps commonOps) (generate.Option, *config.VersionContract, error) {
-	var genOption generate.Option
-
-	var versionContract *config.VersionContract
-
-	if cOps.talosVersion != "latest" {
-		versionContract, err := config.ParseContractFromVersion(cOps.talosVersion)
-		if err != nil {
-			return nil, nil, fmt.Errorf("error parsing Talos version %q: %w", cOps.talosVersion, err)
-		}
-
-		genOption = generate.WithVersionContract(versionContract)
+func getVersionContractGenOps(cOps commonOps) ([]generate.Option, *config.VersionContract, error) {
+	if cOps.talosVersion == "latest" {
+		return nil, nil, nil
 	}
 
-	return genOption, versionContract, nil
+	versionContract, err := config.ParseContractFromVersion(cOps.talosVersion)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error parsing Talos version %q: %w", cOps.talosVersion, err)
+	}
+
+	return []generate.Option{generate.WithVersionContract(versionContract)}, versionContract, nil
 }
 
 func getWithAdditionalSubjectAltNamesGenOps(endpointList []string) []generate.Option {
-	ops := []generate.Option{}
-
-	for _, endpointHostPort := range endpointList {
+	return xslices.Map(endpointList, func(endpointHostPort string) generate.Option {
 		endpointHost, _, err := net.SplitHostPort(endpointHostPort)
 		if err != nil {
 			endpointHost = endpointHostPort
 		}
 
-		ops = append(ops, generate.WithAdditionalSubjectAltNames([]string{endpointHost}))
-	}
-
-	return ops
+		return generate.WithAdditionalSubjectAltNames([]string{endpointHost})
+	})
 }
 
 func getIps(cidr netip.Prefix, cOps commonOps) ([]netip.Addr, error) {
@@ -418,13 +408,7 @@ func postCreate(
 		}
 	}
 
-	fmt.Println("Bootstrapping kubernetes cluster")
-
-	if err := bootstrapCluster(ctx, clusterAccess, cOps); err != nil {
-		return err
-	}
-
-	return nil
+	return bootstrapCluster(ctx, clusterAccess, cOps)
 }
 
 func bootstrapCluster(ctx context.Context, clusterAccess *access.Adapter, cOps commonOps) error {
@@ -448,17 +432,15 @@ func bootstrapCluster(ctx context.Context, clusterAccess *access.Adapter, cOps c
 		checks = slices.Concat(check.PreBootSequenceChecks(), check.K8sComponentsReadinessChecks())
 	}
 
-	checks = append(checks, check.ExtraClusterChecks()...)
+	checks = slices.Concat(checks, check.ExtraClusterChecks())
 
 	if err := check.Wait(checkCtx, clusterAccess, checks, check.StderrReporter()); err != nil {
 		return err
 	}
 
-	if !cOps.skipKubeconfig {
-		if err := mergeKubeconfig(ctx, clusterAccess); err != nil {
-			return err
-		}
+	if cOps.skipKubeconfig {
+		return nil
 	}
 
-	return nil
+	return mergeKubeconfig(ctx, clusterAccess)
 }

cmd/talosctl/cmd/mgmt/cluster/create/create_dev.go
@@ -338,7 +338,7 @@ func create(ctx context.Context, ops createOps) error {
 			return err
 		}
 
-		genOptions = append(genOptions, versionContractGenOps)
+		genOptions = append(genOptions, versionContractGenOps...)
 
 		extraDisks, userVolumePatches, err := getExtraDisks(provisioner, cidr4, versionContract, &provisionOptions, qOps)
 		if err != nil {

cmd/talosctl/cmd/mgmt/cluster/create/create_docker.go
@@ -68,7 +68,7 @@ func getDockerClusterRequest(
 
 	clusterRequest := getBaseClusterRequest(cOps, []netip.Prefix{cidr}, []netip.Addr{gatewayIP})
 
-	genOptions := []generate.Option{}
+	var genOptions []generate.Option
 
 	registryMirrorOps, err := getRegistryMirrorGenOps(cOps)
 	if err != nil {
@@ -83,7 +83,7 @@ func getDockerClusterRequest(
 		return clusterCreateRequestData{}, err
 	}
 
-	genOptions = append(genOptions, versionContractGenOps)
+	genOptions = append(genOptions, versionContractGenOps...)
 	genOptions = append(genOptions, provisioner.GenOptions(clusterRequest.Network)...)
 
 	endpointList := provisioner.GetTalosAPIEndpoints(clusterRequest.Network)

pkg/cluster/bootstrap.go
@@ -50,7 +50,7 @@ func (s *APIBootstrapper) Bootstrap(ctx context.Context, out io.Writer) error {
 	nodeIP := controlPlaneNodes[0].IPs[0]
 	nodeCtx := client.WithNodes(ctx, nodeIP.String())
 
-	fmt.Fprintln(out, "waiting for API")
+	fmt.Fprintln(out, "waiting for Talos API (to bootstrap the cluster)")
 
 	err = retry.Constant(10*time.Minute, retry.WithUnits(500*time.Millisecond)).RetryWithContext(nodeCtx, func(nodeCtx context.Context) error {
 		retryCtx, cancel := context.WithTimeout(nodeCtx, 2*time.Second)