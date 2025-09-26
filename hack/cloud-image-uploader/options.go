33714b7158a0d569be1d0b1d7b012280856db484
Mateusz Urbanek <mateusz.urbanek@siderolabs.com>
2025-09-26 11:34:50 +0200

    feat: release cloud image using factory
    Instead of building images, fetch them from image factory to ensure
    proper schematic is used for including default extensions.
    
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek@siderolabs.com>

--- hack/cloud-image-uploader/options.go
+++ hack/cloud-image-uploader/options.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 )
 
 // Options for the cli.
@@ -16,6 +17,11 @@ type Options struct {
 	NamePrefix    string
 	Architectures []string
 	TargetClouds  []string
+
+	// ImageFactory options.
+	UseFactory        bool
+	FactoryHost       string
+	FactorySchematics []string
 
 	// AWS options.
 	AWSRegions   []string
@@ -27,6 +33,10 @@ var DefaultOptions = Options{
 	ArtifactsPath: "_out/",
 	Architectures: []string{"amd64", "arm64"},
 	TargetClouds:  []string{"aws"},
+	FactoryHost:   "https://factory.talos.dev",
+	FactorySchematics: []string{
+		"aws:10e276a06c1f86b182757a962258ac00655d3425e5957f617bdc82f06894e39b",
+	},
 }
 
 // AWSImage returns path to AWS pre-built image.
@@ -37,4 +47,23 @@ func (o *Options) AWSImage(architecture string) string {
 // GCPImage returns path to GCP pre-built image.
 func (o *Options) GCPImage(architecture string) string {
 	return filepath.Join(o.ArtifactsPath, fmt.Sprintf("gcp-%s.raw.tar.gz", architecture))
+}
+
+// SchematicFor returns the schematic for the given cloud.
+func (o *Options) SchematicFor(cloud string) string {
+	for _, schematic := range o.FactorySchematics {
+		parts := strings.Split(schematic, ":")
+		if len(parts) != 2 {
+			continue
+		}
+
+		cloudPart := parts[0]
+		schematicPart := parts[1]
+
+		if cloudPart == cloud {
+			return schematicPart
+		}
+	}
+
+	return ""
 }
