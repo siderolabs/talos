3472d6e79caa13fd42df7774101397b0a30f62f5
Noel Georgi <git@frezbo.dev>
2025-10-02 12:02:29 +0530

    fix: revert "chore: use new mount/v3 package in efivarfs"
    This reverts commit c6279e04c45504af243c0aef9f255317426b4ca0.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- internal/pkg/efivarfs/efivarfs.go
+++ internal/pkg/efivarfs/efivarfs.go
@@ -23,9 +23,9 @@ import (
 
 	"github.com/g0rbe/go-chattr"
 	"github.com/google/uuid"
+	"golang.org/x/sys/unix"
 	"golang.org/x/text/encoding/unicode"
 
-	"github.com/siderolabs/talos/internal/pkg/mount/v3"
 	"github.com/siderolabs/talos/pkg/machinery/constants"
 )
 
@@ -101,35 +101,26 @@ type ReadWriter interface {
 // FilesystemReaderWriter implements ReaderWriter using the efivars Linux filesystem.
 type FilesystemReaderWriter struct {
 	write bool
-
-	point *mount.Point
 }
 
 // NewFilesystemReaderWriter creates a new FilesystemReaderWriter.
 func NewFilesystemReaderWriter(write bool) (*FilesystemReaderWriter, error) {
-	fsReaderWriter := &FilesystemReaderWriter{
-		write: write,
-	}
-
 	if write {
-		point, err := mount.NewManager(mount.WithTarget(constants.EFIVarsMountPoint),
-			mount.WithFsopen("efivarfs"),
-		).Mount()
-		if err != nil {
-			return nil, fmt.Errorf("remounting efivarfs read-write: %w", err)
+		if err := unix.Mount("efivarfs", constants.EFIVarsMountPoint, "efivarfs", unix.MS_REMOUNT, ""); err != nil {
+			return nil, err
 		}
-
-		fsReaderWriter.point = point
 	}
 
-	return fsReaderWriter, nil
+	return &FilesystemReaderWriter{
+		write: write,
+	}, nil
 }
 
 // Close unmounts efivarfs if the FilesystemReaderWriter was created with write
 // access.
 func (rw *FilesystemReaderWriter) Close() error {
 	if rw.write {
-		return rw.point.Unmount(mount.UnmountOptions{})
+		return unix.Mount("efivarfs", constants.EFIVarsMountPoint, "efivarfs", unix.MS_REMOUNT|unix.MS_RDONLY, "")
 	}
 
 	return nil
