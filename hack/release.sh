51bcfb567915d2b27e4b5321e080220bc618086b
Justin Garrison <justin.garrison@siderolabs.com>
2025-12-01 15:32:16 -0800

    feat: rename image default and source bundle
    s/default/k8s-bundle
    s/source-bundle/talos-bundle
    
    for UX consistency when generating lists of images used by talos.
    
    Remove non-k8s images from k8s-bundle list.
    
    Signed-off-by: Justin Garrison <justin.garrison@siderolabs.com>

--- hack/release.sh
+++ hack/release.sh
@@ -22,7 +22,7 @@ function release-notes {
   release-tool "${2}" --gfm > "${1}"
 
   echo -e '\n## Images\n\n```' >> ${1}
-  ${ARTIFACTS}/talosctl-linux-amd64 image default >> ${1}
+  ${ARTIFACTS}/talosctl-linux-amd64 image k8s-bundle >> ${1}
   echo -e '```\n' >> ${1}
 }
 
