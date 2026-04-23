a75ce6f001820b4d8137c9c4719358f6527eb34b
Noel Georgi <git@frezbo.dev>
2026-04-23 19:52:54 +0530

    feat: bump pkgs, tools
    Bump `PKGS` and `TOOLS`
    
    Kernel: 6.18.24
    Containerd: v2.1.7
    Go: 1.25.9
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- Makefile
+++ Makefile
@@ -25,9 +25,9 @@ DEBUG_TOOLS_SOURCE := scratch
 EMBED_TARGET ?= embed
 
 TOOLS_PREFIX ?= ghcr.io/siderolabs/tools
-TOOLS ?= v1.12.0-7-g57916cb
+TOOLS ?= v1.12.0-10-gbbd753d
 PKGS_PREFIX ?= ghcr.io/siderolabs
-PKGS ?= v1.12.0-50-ga92bed5
+PKGS ?= v1.12.0-56-g191632c
 GENERATE_VEX_PREFIX ?= ghcr.io/siderolabs/generate-vex
 GENERATE_VEX ?= latest
 
--- pkg/machinery/constants/constants.go
+++ pkg/machinery/constants/constants.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// DefaultKernelVersion is the default Linux kernel version.
-	DefaultKernelVersion = "6.18.18-talos"
+	DefaultKernelVersion = "6.18.24-talos"
 
 	// KernelParamConfig is the kernel parameter name for specifying the URL.
 	// to the config.
@@ -561,7 +561,7 @@ const (
 	TrustdUserID = 51
 
 	// DefaultContainerdVersion is the default container runtime version.
-	DefaultContainerdVersion = "2.1.6"
+	DefaultContainerdVersion = "2.1.7"
 
 	// RuncVersion is the runc version.
 	RuncVersion = "1.3.5"
@@ -1131,7 +1131,7 @@ const (
 	DBusClientSocketLabel = "system_u:object_r:dbus_client_socket_t:s0"
 
 	// GoVersion is the version of Go compiler this release was built with.
-	GoVersion = "go1.25.8"
+	GoVersion = "go1.25.9"
 
 	// KubernetesTalosAPIServiceName is the name of the Kubernetes service to access Talos API.
 	KubernetesTalosAPIServiceName = "talos"
