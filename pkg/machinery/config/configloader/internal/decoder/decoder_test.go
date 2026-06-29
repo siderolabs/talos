6f481b420c9494918ecd1885ac218d0b31ac9866
Mateusz Urbanek <mateusz.urbanek@siderolabs.com>
2026-06-29 14:17:00 +0200

    fix: decode extraArgs list values correctly
    siderolabs/gen v0.8.7 includes the list-value
    fix from https://github.com/siderolabs/gen/pull/33. Bump to unblock multi-value
    extraArgs in KubeAPIServerConfig decoder.
    
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek@siderolabs.com>

--- pkg/machinery/config/configloader/internal/decoder/decoder_test.go
+++ pkg/machinery/config/configloader/internal/decoder/decoder_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/siderolabs/talos/pkg/machinery/config/config"
 	"github.com/siderolabs/talos/pkg/machinery/config/configloader/internal/decoder"
 	"github.com/siderolabs/talos/pkg/machinery/config/internal/registry"
+	"github.com/siderolabs/talos/pkg/machinery/config/types/k8s"
 	"github.com/siderolabs/talos/pkg/machinery/config/types/meta"
 	"github.com/siderolabs/talos/pkg/machinery/config/types/v1alpha1"
 )
@@ -303,6 +304,28 @@ pods:
 `),
 			expected:    nil,
 			expectedErr: "",
+		},
+		{
+			name: "kube apiserver extra args list value",
+			source: []byte(`---
+apiVersion: v1alpha1
+kind: KubeAPIServerConfig
+extraArgs:
+  service-account-issuer:
+    - https://OLD-ENDPOINT:6443
+    - https://NEW-ENDPOINT:6443
+`),
+			expected: []config.Document{
+				&k8s.KubeAPIServerConfigV1Alpha1{
+					Meta: meta.Meta{
+						MetaAPIVersion: "v1alpha1",
+						MetaKind:       "KubeAPIServerConfig",
+					},
+					PodArgs: meta.Args{
+						"service-account-issuer": meta.NewArgValue("", []string{"https://OLD-ENDPOINT:6443", "https://NEW-ENDPOINT:6443"}),
+					},
+				},
+			},
 		},
 		{
 			name: "omit empty test",
