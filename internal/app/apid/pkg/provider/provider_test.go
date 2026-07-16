cf67bcd87424716729f84b6c160684826fdef5b8
neiltalap <contact@neiltalap.com>
2026-07-16 14:33:38 +0500

    test: apid tls provider public surface
    Test only exported TLSConfig APIs.
    
    Signed-off-by: neiltalap <contact@neiltalap.com>

--- internal/app/apid/pkg/provider/provider_test.go
+++ internal/app/apid/pkg/provider/provider_test.go
@@ -2,27 +2,50 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-package provider
+package provider_test
 
 import (
+	"context"
 	stdlibtls "crypto/tls"
 	"testing"
+	"time"
 
+	"github.com/cosi-project/runtime/pkg/resource"
+	"github.com/cosi-project/runtime/pkg/state"
+	"github.com/cosi-project/runtime/pkg/state/impl/inmem"
+	"github.com/cosi-project/runtime/pkg/state/impl/namespaced"
 	"github.com/siderolabs/crypto/x509"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/siderolabs/talos/internal/app/apid/pkg/provider"
 	"github.com/siderolabs/talos/pkg/machinery/resources/secrets"
 )
 
-type CertificateProviderSuite struct {
+type TLSConfigSuite struct {
 	suite.Suite
+
+	ctx       context.Context //nolint:containedctx
+	ctxCancel context.CancelFunc
+
+	resources state.State
 }
 
-func TestCertificateProviderSuite(t *testing.T) {
-	suite.Run(t, new(CertificateProviderSuite))
+func TestTLSConfigSuite(t *testing.T) {
+	suite.Run(t, new(TLSConfigSuite))
 }
 
-func (suite *CertificateProviderSuite) newAPICerts(withClient bool) (*secrets.API, []byte) {
+func (suite *TLSConfigSuite) SetupTest() {
+	suite.ctx, suite.ctxCancel = context.WithTimeout(suite.T().Context(), 10*time.Second)
+	suite.resources = state.WrapCore(namespaced.NewState(inmem.Build))
+}
+
+func (suite *TLSConfigSuite) TearDownTest() {
+	suite.ctxCancel()
+}
+
+// newAPICerts builds a secrets.API resource with a fresh CA and server cert.
+// When withClient is true, a client cert is included as well.
+func (suite *TLSConfigSuite) newAPICerts(withClient bool) *secrets.API {
 	ca, err := x509.NewSelfSignedCertificateAuthority()
 	suite.Require().NoError(err)
 
@@ -40,189 +63,144 @@ func (suite *CertificateProviderSuite) newAPICerts(withClient bool) (*secrets.AP
 		api.TypedSpec().Client = x509.NewCertificateAndKeyFromKeyPair(clientCrt)
 	}
 
-	return api, ca.CrtPEM
+	return api
 }
 
-func (suite *CertificateProviderSuite) TestUpdate() {
-	api, caPEM := suite.newAPICerts(true)
+// newTLSConfig seeds the state with API certs and returns a ready TLSConfig.
+func (suite *TLSConfigSuite) newTLSConfig(withClient, skipClientCertVerify bool) *provider.TLSConfig {
+	suite.Require().NoError(suite.resources.Create(suite.ctx, suite.newAPICerts(withClient)))
 
-	p := &certificateProvider{}
-	suite.Require().NoError(p.Update(api))
-
-	suite.True(p.HasClientCertificate())
-
-	caBytes, err := p.GetCA()
+	cfg, err := provider.NewTLSConfig(suite.ctx, suite.resources, skipClientCertVerify)
 	suite.Require().NoError(err)
-	suite.Equal(caPEM, caBytes)
 
-	pool, err := p.GetCACertPool()
-	suite.Require().NoError(err)
-	suite.NotNil(pool)
-
-	serverCert, err := p.GetCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotNil(serverCert)
-
-	clientCert, err := p.GetClientCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotNil(clientCert)
+	return cfg
 }
 
-func (suite *CertificateProviderSuite) TestUpdateNoClient() {
-	api, _ := suite.newAPICerts(false)
-
-	p := &certificateProvider{}
-	suite.Require().NoError(p.Update(api))
-
-	suite.False(p.HasClientCertificate())
-
-	clientCert, err := p.GetClientCertificate(nil)
-	suite.Require().NoError(err)
-	suite.Nil(clientCert)
-
-	serverCert, err := p.GetCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotNil(serverCert)
-}
-
-func (suite *CertificateProviderSuite) TestUpdateInvalidServerKeyPair() {
-	api, _ := suite.newAPICerts(true)
-	api.TypedSpec().Server.Key = []byte("not-a-key")
-
-	p := &certificateProvider{}
-	suite.Error(p.Update(api))
-}
-
-func (suite *CertificateProviderSuite) TestUpdateInvalidClientKeyPair() {
-	api, _ := suite.newAPICerts(true)
-	api.TypedSpec().Client.Key = []byte("not-a-key")
-
-	p := &certificateProvider{}
-	suite.Error(p.Update(api))
-}
-
-func (suite *CertificateProviderSuite) TestUpdateInvalidCA() {
-	api, _ := suite.newAPICerts(true)
-	api.TypedSpec().AcceptedCAs = []*x509.PEMEncodedCertificate{
-		{Crt: []byte("not-a-cert")},
-	}
-
-	p := &certificateProvider{}
-	suite.Error(p.Update(api))
-}
-
-func (suite *CertificateProviderSuite) TestUpdateEmptyCA() {
-	api, _ := suite.newAPICerts(true)
-	api.TypedSpec().AcceptedCAs = nil
-
-	p := &certificateProvider{}
-	suite.Require().NoError(p.Update(api))
-
-	caBytes, err := p.GetCA()
-	suite.Require().NoError(err)
-	suite.Empty(caBytes)
-}
-
-func (suite *CertificateProviderSuite) TestUpdateClearsClient() {
-	p := &certificateProvider{}
-
-	apiWithClient, _ := suite.newAPICerts(true)
-	suite.Require().NoError(p.Update(apiWithClient))
-	suite.True(p.HasClientCertificate())
-
-	apiWithoutClient, _ := suite.newAPICerts(false)
-	suite.Require().NoError(p.Update(apiWithoutClient))
-	suite.False(p.HasClientCertificate())
-
-	clientCert, err := p.GetClientCertificate(nil)
-	suite.Require().NoError(err)
-	suite.Nil(clientCert)
-}
-
-func (suite *CertificateProviderSuite) TestUpdateRotation() {
-	p := &certificateProvider{}
-
-	api1, _ := suite.newAPICerts(true)
-	suite.Require().NoError(p.Update(api1))
-
-	server1, err := p.GetCertificate(nil)
+// updateAPI applies mutate to the current secrets.API resource in state.
+func (suite *TLSConfigSuite) updateAPI(mutate func(*secrets.API)) {
+	r, err := suite.resources.Get(suite.ctx, resource.NewMetadata(secrets.NamespaceName, secrets.APIType, secrets.APIID, resource.VersionUndefined))
 	suite.Require().NoError(err)
 
-	client1, err := p.GetClientCertificate(nil)
-	suite.Require().NoError(err)
-
-	api2, _ := suite.newAPICerts(true)
-	suite.Require().NoError(p.Update(api2))
-
-	server2, err := p.GetCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotEqual(server1.Certificate[0], server2.Certificate[0])
-
-	client2, err := p.GetClientCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotEqual(client1.Certificate[0], client2.Certificate[0])
+	api := r.(*secrets.API).DeepCopy().(*secrets.API) //nolint:forcetypeassert
+	mutate(api)
+	suite.Require().NoError(suite.resources.Update(suite.ctx, api))
 }
 
-func (suite *CertificateProviderSuite) newTLSConfig(withClient, skipClientCertVerify bool) *TLSConfig {
-	api, _ := suite.newAPICerts(withClient)
-
-	p := &certificateProvider{}
-	suite.Require().NoError(p.Update(api))
-
-	return &TLSConfig{
-		certificateProvider:  p,
-		skipClientCertVerify: skipClientCertVerify,
-	}
-}
-
-func (suite *CertificateProviderSuite) TestServerConfigMutual() {
+// TestServerConfigMutual checks mutual TLS client auth when skipClientCertVerify is false.
+func (suite *TLSConfigSuite) TestServerConfigMutual() {
 	cfg := suite.newTLSConfig(true, false)
 
 	serverTLS, err := cfg.ServerConfig()
 	suite.Require().NoError(err)
-	suite.Require().NotNil(serverTLS)
-
 	suite.Equal(stdlibtls.RequireAndVerifyClientCert, serverTLS.ClientAuth)
-	suite.NotNil(serverTLS.GetCertificate)
-	suite.NotNil(serverTLS.GetConfigForClient)
 
 	serverCert, err := serverTLS.GetCertificate(nil)
 	suite.Require().NoError(err)
 	suite.NotNil(serverCert)
 }
 
-func (suite *CertificateProviderSuite) TestServerConfigServerOnly() {
+// TestServerConfigServerOnly checks server-only TLS when skipClientCertVerify is true.
+func (suite *TLSConfigSuite) TestServerConfigServerOnly() {
 	cfg := suite.newTLSConfig(true, true)
 
 	serverTLS, err := cfg.ServerConfig()
 	suite.Require().NoError(err)
-	suite.Require().NotNil(serverTLS)
-
 	suite.Equal(stdlibtls.NoClientCert, serverTLS.ClientAuth)
-	suite.NotNil(serverTLS.GetCertificate)
-
-	serverCert, err := serverTLS.GetCertificate(nil)
-	suite.Require().NoError(err)
-	suite.NotNil(serverCert)
 }
 
-func (suite *CertificateProviderSuite) TestClientConfigWithClient() {
+// TestClientConfigWithClient checks ClientConfig when a client cert is present.
+func (suite *TLSConfigSuite) TestClientConfigWithClient() {
 	cfg := suite.newTLSConfig(true, false)
 
 	clientTLS, err := cfg.ClientConfig()
 	suite.Require().NoError(err)
 	suite.Require().NotNil(clientTLS)
 
-	suite.NotNil(clientTLS.GetClientCertificate)
-	suite.NotNil(clientTLS.RootCAs)
-
 	clientCert, err := clientTLS.GetClientCertificate(nil)
 	suite.Require().NoError(err)
 	suite.NotNil(clientCert)
 }
 
-func (suite *CertificateProviderSuite) TestClientConfigWithoutClient() {
+// TestClientConfigWithoutClient checks that ClientConfig is nil without a client cert.
+func (suite *TLSConfigSuite) TestClientConfigWithoutClient() {
 	cfg := suite.newTLSConfig(false, false)
+
+	clientTLS, err := cfg.ClientConfig()
+	suite.Require().NoError(err)
+	suite.Nil(clientTLS)
+}
+
+// TestWatchRotation checks that Watch reloads server and client certs on API updates.
+func (suite *TLSConfigSuite) TestWatchRotation() {
+	cfg := suite.newTLSConfig(true, false)
+
+	serverTLS, err := cfg.ServerConfig()
+	suite.Require().NoError(err)
+
+	server1, err := serverTLS.GetCertificate(nil)
+	suite.Require().NoError(err)
+
+	clientTLS, err := cfg.ClientConfig()
+	suite.Require().NoError(err)
+
+	client1, err := clientTLS.GetClientCertificate(nil)
+	suite.Require().NoError(err)
+
+	updated := make(chan struct{}, 1)
+
+	go func() {
+		_ = cfg.Watch(suite.ctx, func() {
+			select {
+			case updated <- struct{}{}:
+			default:
+			}
+		})
+	}()
+
+	next := suite.newAPICerts(true)
+	suite.updateAPI(func(api *secrets.API) {
+		*api.TypedSpec() = *next.TypedSpec()
+	})
+
+	select {
+	case <-updated:
+	case <-suite.ctx.Done():
+		suite.Require().Fail("timed out waiting for watch update")
+	}
+
+	server2, err := serverTLS.GetCertificate(nil)
+	suite.Require().NoError(err)
+	suite.NotEqual(server1.Certificate[0], server2.Certificate[0])
+
+	client2, err := clientTLS.GetClientCertificate(nil)
+	suite.Require().NoError(err)
+	suite.NotEqual(client1.Certificate[0], client2.Certificate[0])
+}
+
+// TestWatchClearsClient checks that removing the client cert makes ClientConfig nil.
+func (suite *TLSConfigSuite) TestWatchClearsClient() {
+	cfg := suite.newTLSConfig(true, false)
+
+	updated := make(chan struct{}, 1)
+
+	go func() {
+		_ = cfg.Watch(suite.ctx, func() {
+			select {
+			case updated <- struct{}{}:
+			default:
+			}
+		})
+	}()
+
+	suite.updateAPI(func(api *secrets.API) {
+		api.TypedSpec().Client = nil
+	})
+
+	select {
+	case <-updated:
+	case <-suite.ctx.Done():
+		suite.Require().Fail("timed out waiting for watch update")
+	}
 
 	clientTLS, err := cfg.ClientConfig()
 	suite.Require().NoError(err)
