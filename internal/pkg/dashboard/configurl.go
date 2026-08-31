6ee461278e53d22972289b096eb2bca86d7b7eb7
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2026-08-31 23:32:31 +0400

    fix: escape output in the talosctl dashboard
    The output to tview might be re-processed by tview for its own color
    tags, so ensure any input data is escaped before drawn into tview.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- internal/pkg/dashboard/configurl.go
+++ internal/pkg/dashboard/configurl.go
@@ -80,7 +80,7 @@ func NewConfigURLGrid(ctx context.Context, dashboard *Dashboard) *ConfigURLGrid 
 
 		err := dashboard.cli.MetaWrite(ctx, meta.DownloadURLCode, []byte(value))
 		if err != nil {
-			grid.infoView.SetText(fmt.Sprintf("[red]Error: %v[-]", err))
+			grid.infoView.SetText(fmt.Sprintf("[red]Error: %v[-]", tview.Escape(err.Error())))
 
 			return
 		}
@@ -100,7 +100,7 @@ func NewConfigURLGrid(ctx context.Context, dashboard *Dashboard) *ConfigURLGrid 
 				return
 			}
 
-			grid.infoView.SetText(fmt.Sprintf("[red]Error: %v[-]", err))
+			grid.infoView.SetText(fmt.Sprintf("[red]Error: %v[-]", tview.Escape(err.Error())))
 
 			return
 		}
@@ -180,7 +180,7 @@ func (widget *ConfigURLGrid) updateNodeData(data resourcedata.Data) {
 					val = "(empty)"
 				}
 
-				nodeData.existingCode = fmt.Sprintf("[blue]%s[-]", val)
+				nodeData.existingCode = fmt.Sprintf("[blue]%s[-]", tview.Escape(val))
 			}
 		}
 	}
