c7ac6acf88f5759e4efc4d26fb8dcd9de01ea8b6
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2026-09-01 21:03:40 +0400

    fix: treat desired roles empty as error in Talos API access
    In Talos API access from K8s, the empty desired roles on the service
    account CRD was treated as normal, which leads to talosconfig issues
    with no roles at all, which on its own makes no sense, as RBAC is
    enforced (and Talos API access from Kubernetes requires RBAC,
    and now RBAC is locked on always).
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>
    (cherry picked from commit b1b173f598a41be77146e7a4a7daee2747b4a7fa)

--- internal/app/machined/pkg/controllers/kubeaccess/serviceaccount/crd_controller.go
+++ internal/app/machined/pkg/controllers/kubeaccess/serviceaccount/crd_controller.go
@@ -321,7 +321,11 @@ func (t *CRDController) syncHandler(ctx context.Context, key string) error {
 	}
 
 	desiredRoles, found, err := unstructured.NestedStringSlice(talosSA.UnstructuredContent(), "spec", "roles")
-	if err != nil || !found {
+
+	// parse desiredRoles to see if they are empty.
+	desiredRoleSet, _ := role.Parse(desiredRoles)
+
+	if err != nil || !found || desiredRoleSet.Empty() {
 		msg := messageRolesNotFound
 
 		updateErr := t.updateTalosSAStatus(ctx, talosSA, msg)
@@ -338,8 +342,6 @@ func (t *CRDController) syncHandler(ctx context.Context, key string) error {
 		return errors.New(msg)
 	}
 
-	desiredRoleSet, _ := role.Parse(desiredRoles)
-
 	if !slices.ContainsFunc(t.allowedNamespaces, func(allowedNS string) bool {
 		return allowedNS == namespace
 	}) {
@@ -355,6 +357,7 @@ func (t *CRDController) syncHandler(ctx context.Context, key string) error {
 		return nil
 	}
 
+	// every requested role must appear in the allowlist; the empty desiredRoles list is handled above.
 	var unallowedRoles []string
 
 	for _, desiredRole := range desiredRoles {
